827707f36fa2f73730e29c7d17d37595d9263af8
mike-goodwin <mike.goodwin@sage.com>
2015-10-10 02:47:52 +0100

    missing karma config file

--- karma.conf.js
+++ karma.conf.js
@@ -2,51 +2,55 @@
 // Generated on Thu Jun 18 2015 22:02:08 GMT+0100 (GMT Daylight Time)
 
 module.exports = function(config) {
-  config.set({
+    config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-        'td/public/libs/angular/angular.js',
-        'td/public/libs/angular-route/angular-route.js',
-        'td/public/libs/jquery/jquery.js',
-        'td/public/libs/bootstrap/bootstrap.js',
-        'td/public/libs/toastr/toastr.js',
-        'td/public/libs/lodash/lodash.js',
-        'td/public/libs/backbone/backbone.js',
-        'td/public/libs/joint/joint.js',
-        'td/public/libs/angular-ui-bootstrap-bower/ui-bootstrap-tpls.js',
-        'td/public/libs/angular-mocks/angular-mocks.js',
-        'td/public/app/*.js',
-        'td/public/app/**/*.js',
-        'td.tests/spec/*.js'
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            'td/public/libs/angular/angular.js',
+            'td/public/libs/angular-route/angular-route.js',
+            'td/public/libs/jquery/jquery.js',
+            'td/public/libs/bootstrap/bootstrap.js',
+            'td/public/libs/toastr/toastr.js',
+            'td/public/libs/lodash/lodash.js',
+            'td/public/libs/backbone/backbone.js',
+            'td/public/libs/joint/joint.js',
+            'td/public/libs/angular-ui-bootstrap-bower/ui-bootstrap-tpls.js',
+            'td/public/libs/angular-mocks/angular-mocks.js',
+            'td/public/app/*.js',
+            'td/public/app/**/*.js',
+            'td.tests/spec/*.js'
+        ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        // list of files to exclude
+        exclude: [
+        ],
+
+
+        // preprocess matching files before serving them to the browser
+        // available preprocesors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'td/public/app/**/*.js': ['coverage']
+        },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'xml'],
+        reporters: ['progress', 'xml', 'coverage'],
 
+        coverageReporter: {
+            reporters: [{ type: 'lcov' }]
+        },
 
     // web server port
     port: 5858,
@@ -67,7 +71,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome', 'Firefox', 'IE'],
 
 
     // Continuous Integration mode
