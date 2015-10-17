a6ef4830a63601c8a2ec5a5d7b813440c454d702
mike-goodwin <mike.goodwin@sage.com>
2015-10-17 21:31:22 +0100

    datacontext unit tests over 90% coverage

--- td.tests/spec/datacontext_spec.js
+++ td.tests/spec/datacontext_spec.js
@@ -49,6 +49,7 @@ describe('datacontext service:', function () {
         var model = [];
         var diagram =[];
         var elementProperties = [];
+        var threats = [];
         var diagrams;
         var initialModels;
 
@@ -63,10 +64,28 @@ describe('datacontext service:', function () {
 
             diagrams = [diagram[0], diagram[1], diagram[2]];
 
+            var threatKey = 0;
+
             for (var i = 0; i < 4; i++) {
 
                 detail[i] = { description: 'desc' + i, diagrams: diagrams };
-                elementProperties[i] = ['properties' + i + '0', 'properties' + i + '1', 'properties' + i + '2'];
+
+                threats[i] = [];
+
+                for (var j = 0; j < 3; j++) {
+
+                    threats[i][j] = { id: threatKey, title: 'threat' + i + '-' + j };
+                    threatKey++;
+
+                }
+
+                elementProperties[i] = {};
+
+                for (var j = 0; j < 3; j++) {
+
+                    elementProperties[i][i + '-' + j] = { elementId: i + '-' + j, threats: threats[i], threatModelId: i, diagramId: i, prop: 'propValue' }
+
+                }
 
             }
 
@@ -77,31 +96,6 @@ describe('datacontext service:', function () {
 
             initialModels = [model[0], model[1], model[2]];
             localStorage.setItem('models', JSON.stringify(initialModels));
-
-
-            //diagram0 = { size: 'size0', diagramJson: 'diagramJson0' };
-            //diagram1 = { size: 'size1', diagramJson: 'diagramJson1' };
-            //diagram2 = { size: 'size2', diagramJson: 'diagramJson2' };
-            //diagram3 = { size: 'size3', diagramJson: 'diagramJson3' };
-            //diagrams = [diagram0, diagram1, diagram2];
-            //summary0 = { id: 0, title: 'summary0' };
-            //summary1 = { id: 1, title: 'summary1' };
-            //summary2 = { id: 2, title: 'summary2' };
-            //summary3 = { id: 2, title: 'summary3' }; //spare
-            //detail0 = { description: 'desc0', diagrams: diagrams };
-            //detail1 = { description: 'desc1', diagrams: diagrams };
-            //detail2 = { description: 'desc2', diagrams: diagrams };
-            //detail3 = { description: 'desc3', diagrams: diagrams }; //spare
-            //elementProperties0 = ['properties00', 'properties01', 'properties02'];
-            //elementProperties1 = ['properties10', 'properties11', 'properties12'];
-            //elementProperties2 = ['properties20', 'properties21', 'properties22'];
-            //elementProperties3 = ['properties30', 'properties31', 'properties32'];
-            //model0 = { summary: summary0, detail: detail0, elementProperties: elementProperties0 };
-            //model1 = { summary: summary1, detail: detail1, elementProperties: elementProperties1 };
-            //model2 = { summary: summary2, detail: detail2, elementProperties: elementProperties2 };
-            //model3 = { summary: summary3, detail: detail3, elementProperties: elementProperties3 }; //spare
-            //initialModels = [model0, model1, model2]; //dont add the spare
-            //localStorage.setItem('models', JSON.stringify(initialModels));
 
             angular.mock.inject(function (_datacontext_) {
                 datacontext = _datacontext_;
@@ -300,9 +294,9 @@ describe('datacontext service:', function () {
 
         it('should get the specified element properties', function (done) {
 
-            datacontext.getElementProperties(1, 1, 1).then(function (properties) {
+            datacontext.getElementProperties(1, 1, '1-1').then(function (properties) {
 
-                expect(properties).toEqual('properties11');
+                expect(properties).toEqual(elementProperties[1]['1-1']);
                 done();
 
             });
@@ -311,20 +305,46 @@ describe('datacontext service:', function () {
 
         });
 
-        it('should update the element properties', function (done) {
+        it('should update the element properties and return it', function (done) {
 
-            done();
+            elementProperties[1]['1-1'].prop = 'newPropValue';
+            datacontext.saveElementProperties(elementProperties[1]['1-1']).then(function(properties) {
+
+                expect(properties).toEqual(elementProperties[1]['1-1']);
+                expect(JSON.parse(localStorage.getItem('models'))[1].elementProperties['1-1']).toEqual(elementProperties[1]['1-1']);
+                done();
+
+            });
+
+            $rootScope.$apply();
+
+        });
+
+        it('should generate a key for the new threat', function (done) {
+
+            elementProperties[1]['1-1'].threats.push({ title: 'new threat' });
+            datacontext.saveElementProperties(elementProperties[1]['1-1']).then(function (properties) {
+
+                expect(properties.threats[3].id).toBeDefined();
+                expect(JSON.parse(localStorage.getItem('models'))[1].elementProperties['1-1']).toEqual(elementProperties[1]['1-1']);
+                done();
+
+            });
+
+            $rootScope.$apply();
 
         });
 
 
         it('should delete the specified element properties and return them', function (done) {
 
-            datacontext.deleteElementProperties(1, 1, 1).then(function (properties) {
+            datacontext.deleteElementProperties(1, 1, '1-1').then(function (properties) {
 
                 var models = JSON.parse(localStorage.getItem('models'));
-                expect(properties).toEqual('properties11');
-                expect(models[1].elementProperties).toEqual(['properties10',null, 'properties12']);
+                expect(properties).toEqual(elementProperties[1]['1-1']);
+                expect(models[1].elementProperties['0-1']).toEqual(elementProperties[1]['0-1']);
+                expect(models[1].elementProperties['1-1']).toBeUndefined();
+                expect(models[1].elementProperties['2-1']).toEqual(elementProperties[1]['2-1']);
                 done();
 
             });
