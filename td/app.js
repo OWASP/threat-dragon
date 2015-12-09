447eaac0051e37890a5537ec0d25015f66b7b21a
mike-goodwin <mike.goodwin@owaps.org>
2015-12-09 23:11:47 +0000

    some simple security headers

--- td/app.js
+++ td/app.js
@@ -4,19 +4,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var helmet = require('helmet');
 var routes = require('./routes/index');
 
 var app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+//security headers
+app.use(helmet.frameguard('deny'));
+app.use(helmet.hidePoweredBy());
+app.use(helmet.noSniff());
 
 app.use('/', routes);
 
