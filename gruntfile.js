b0f51db21cd79ad46dd8b0375cc7f4c403de74af
mike-goodwin <mike.goodwin@owaps.org>
2016-04-07 23:45:34 +0100

    starting to remove grunt

--- gruntfile.js
+++ gruntfile.js
@@ -1,41 +1,21 @@
 ﻿/// <binding AfterBuild='bower, uglify, cssmin, wiredep' />
-module.exports = function (grunt) {
-    
+module.exports = function(grunt) {
+
     grunt.loadNpmTasks('grunt-wiredep');
-	grunt.renameTask('wiredep', 'wiredepdebug');
-	grunt.loadNpmTasks('grunt-wiredep');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.renameTask('wiredep', 'wiredepdebug');
+    grunt.loadNpmTasks('grunt-wiredep');
     grunt.loadNpmTasks('grunt-bower-installer');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-karma');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-	
-	grunt.registerTask('default', ['bower', 'uglify', 'cssmin', 'wiredep', 'jshint', 'karma']);
-	grunt.registerTask('release', ['bower', 'uglify', 'cssmin', 'wiredep', 'jshint', 'karma']);
-	grunt.registerTask('debug', ['bower', 'wiredepdebug']);
-	grunt.registerTask('test', ['jshint', 'karma']);
-    
+
+    grunt.registerTask('default', ['bower', 'uglify', 'cssmin', 'wiredep']);
+    grunt.registerTask('release', ['bower', 'uglify', 'cssmin', 'wiredep']);
+    grunt.registerTask('debug', ['bower', 'wiredepdebug']);
+
     grunt.initConfig({
-		
+
         pkg: grunt.file.readJSON('package.json'),
 
-        jshint: {
-            options: {
-                jshintrc: true,
-                ignores: ['td.test/', 'td/public/libs/']
-            },
-            files: {
-                src: ['td']
-            }
-        },
-		
-        karma: {
-            unit: {
-                configFile: 'karma.conf.js'
-            }
-        },		
-		
         bower: {
             install: {
                 options: {
@@ -50,23 +30,23 @@ module.exports = function (grunt) {
                 }
             }
         },
-		
+
         uglify: {
             minifyjs: {
                 files: [{
-                        expand: true,
-                        cwd: 'td/public/libs',
-                        src: '**/*.js',
-                        dest: 'td/public/libs',
-                        ext: '.min.js',
-                        extDot: 'first'
-                    }]
+                    expand: true,
+                    cwd: 'td/public/libs',
+                    src: '**/*.js',
+                    dest: 'td/public/libs',
+                    ext: '.min.js',
+                    extDot: 'first'
+                }]
             },
             options: {
                 sourceMap: true
             }
         },
-		
+
         cssmin: {
             options: {
                 shorthandCompacting: false,
@@ -74,16 +54,16 @@ module.exports = function (grunt) {
             },
             minifycss: {
                 files: [{
-                        expand: true,
-                        cwd: 'td/public/libs',
-                        src: '**/*.css',
-                        dest: 'td/public/libs',
-                        ext: '.min.css',
-                        extDot: 'first'
-                    }]
+                    expand: true,
+                    cwd: 'td/public/libs',
+                    src: '**/*.css',
+                    dest: 'td/public/libs',
+                    ext: '.min.css',
+                    extDot: 'first'
+                }]
             }
         },
-		
+
         wiredepdebug: {
             task: {
                 src: ['td/public/index.html'],
@@ -92,21 +72,21 @@ module.exports = function (grunt) {
                     fileTypes: {
                         html: {
                             replace: {
-                                js: function (filePath) {
+                                js: function(filePath) {
                                     var prefixLength = '../../bower-packages'.length;
                                     var trimmedPath = filePath.slice(prefixLength);
                                     var packageName = trimmedPath.slice(1).split('/')[0];
                                     var fileName = trimmedPath.slice(trimmedPath.lastIndexOf('/'));
-                                    
+
                                     return '<script src="libs/' + packageName + fileName + '"></script>';
                                 },
-                                
-                                css: function (filePath) {
+
+                                css: function(filePath) {
                                     var prefixLength = '../../bower-packages'.length;
                                     var trimmedPath = filePath.slice(prefixLength);
                                     var packageName = trimmedPath.slice(1).split('/')[0];
                                     var fileName = trimmedPath.slice(trimmedPath.lastIndexOf('/'));
-                                    
+
                                     return '<link href="libs/' + packageName + fileName + '" rel="stylesheet" />';
                                 }
                             }
@@ -115,8 +95,8 @@ module.exports = function (grunt) {
                 }
             }
         },
-		
-		wiredep: {
+
+        wiredep: {
             task: {
                 src: ['td/index.html'],
                 options: {
@@ -124,23 +104,23 @@ module.exports = function (grunt) {
                     fileTypes: {
                         html: {
                             replace: {
-                                js: function (filePath) {
+                                js: function(filePath) {
                                     var prefixLength = '../bower-packages'.length;
                                     var trimmedPath = filePath.slice(prefixLength);
                                     var packageName = trimmedPath.slice(1).split('/')[0];
                                     var fileName = trimmedPath.slice(trimmedPath.lastIndexOf('/'));
-									var minFileName = fileName.split('.')[0] + '.min.js';
-                                    
+                                    var minFileName = fileName.split('.')[0] + '.min.js';
+
                                     return '<script src="public/libs/' + packageName + minFileName + '"></script>';
                                 },
-                                
-                                css: function (filePath) {
+
+                                css: function(filePath) {
                                     var prefixLength = '../bower-packages'.length;
                                     var trimmedPath = filePath.slice(prefixLength);
                                     var packageName = trimmedPath.slice(1).split('/')[0];
                                     var fileName = trimmedPath.slice(trimmedPath.lastIndexOf('/'));
-									var minFileName = fileName.split('.')[0] + '.min.css';
-                                    
+                                    var minFileName = fileName.split('.')[0] + '.min.css';
+
                                     return '<link href="public/libs/' + packageName + minFileName + '" rel="stylesheet" />';
                                 }
                             }
