3d88914deae6e840c0139098b2eb3c74836d895b
mike-goodwin <mike.goodwin@owaps.org>
2016-04-13 22:49:37 +0100

    encrypted sessions no unit tests

--- td/config/passport.config.js
+++ td/config/passport.config.js
@@ -2,6 +2,7 @@
 
 var passport = require('passport');
 var Strategy = require('passport-github').Strategy;
+var cryptoHelper = require('../helpers/encryption.helper');
 
 function passportConfig(app) {
     
@@ -21,13 +22,20 @@ function passportConfig(app) {
     }));
 
     //serialisation/deserialisation of users
+    //session contains sensitive info like access tokens so encrypt it before storage
+    
+    //encrypt is async to avoid blocking when generating random iv
     passport.serializeUser(function(user, done) {
+        cryptoHelper.encrypt(JSON.stringify(user), function(cipherText) {
+            done(null, cipherText);
+        })
+    });
+    
+    //decrypt is syncronous because there is no blocking code  
+    passport.deserializeUser(function(obj, done) {
+        var user = JSON.parse(cryptoHelper.decrypt(obj));
         done(null, user);
     });
-
-    passport.deserializeUser(function(obj, done) {
-        done(null, obj);
-    }); 
 }
 
 module.exports = passportConfig;
---
+++ td/helpers/encryption.helper.js
@@ -0,0 +1,70 @@
+'user strict';
+
+var crypto = require('crypto');
+var inputEncoding = 'ascii';
+var outputEncoding = 'base64';
+var keyEncoding = 'ascii';
+var algorithm = 'aes256';
+
+function generateIV(cb) {
+    crypto.randomBytes(16, function(err, iv) {
+        cb(iv);
+    });
+}
+
+function getPrimaryKey() {
+    var keys = JSON.parse(process.env.SESSION_ENCRYPTION_KEYS);
+    var primaryKey = keys.find(function(key) { return key.isPrimary; });
+
+    if (!primaryKey) {
+        require('loggers.config').logger.error('missing primary session encryption key');
+    }
+    
+    return {id: primaryKey.id, value: new Buffer(primaryKey.value, keyEncoding)};
+}
+
+function getKeyById(id) {
+    var keys = JSON.parse(process.env.SESSION_ENCRYPTION_KEYS);
+    var key = keys.find(function(key) { return key.id == id; });
+
+    if (!key) {
+        require('loggers.config').logger.error('missing key id ', id);
+    }
+    
+    return {id: key.id, value: new Buffer(key.value, keyEncoding)};       
+}
+
+function encryptData(plainText, key, iv) {
+    var encryptor = crypto.createCipheriv(algorithm, key.value, iv);
+    var cipherText = encryptor.update(plainText, inputEncoding, outputEncoding);
+    cipherText += encryptor.final(outputEncoding);
+    var encryptedData = {keyId: key.id, iv: iv.toString(keyEncoding), data: cipherText};
+    return encryptedData;
+}
+
+function decryptData(cipherText, key, iv) {
+    var decryptor = crypto.createDecipheriv(algorithm, key.value, iv);
+    var plainText = decryptor.update(cipherText, outputEncoding, inputEncoding);
+    plainText += decryptor.final(inputEncoding);
+    return plainText;
+}
+
+function encrypt(plainText, cb) {
+    generateIV(function(iv) {
+        var key = getPrimaryKey();
+        cb(encryptData(plainText, key, iv));
+    });
+}
+
+function decrypt(encryptedData) {
+    var iv = new Buffer(encryptedData.iv, keyEncoding);
+    var key = getKeyById(encryptedData.keyId);
+    return decryptData(encryptedData.data, key, iv);  
+}
+
+var helper = {
+    encrypt: encrypt,
+    decrypt: decrypt
+};
+
+module.exports = helper;
