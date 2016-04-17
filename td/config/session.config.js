046c530fc6ddc8ae7a44379cd7371ff20d80dca3
mike-goodwin <mike.goodwin@owaps.org>
2016-04-17 19:07:50 +0100

    bug in session error log passing

--- td/config/session.config.js
+++ td/config/session.config.js
@@ -6,15 +6,16 @@ var AzureTablesStoreFactory = require('connect-azuretables')(session);
 function configSessions(app) {
     
     var cookieOptions = { maxAge: 3600000 };
+    var logger = require('../config/loggers.config').logger;
     
     if(process.env.NODE_ENV != 'development' && process.env.NODE_ENV) {
         cookieOptions.secure = true;
     } else {
-        require('../config/loggers.config').logger.error({security: true}, 'secure session cookie flag was false - should only happen in dev environments');
+        logger.error({security: true}, 'secure session cookie flag was false - should only happen in dev environments');
     }
 
     app.use(session({
-        store: AzureTablesStoreFactory.create({errorLogger: require('../config/loggers.config').logger.error}),
+        store: AzureTablesStoreFactory.create({errorLogger: logger.error.bind(logger)}),
         secret: process.env.SESSION_SIGNING_KEY,
         resave: false,
         saveUninitialized: false,
