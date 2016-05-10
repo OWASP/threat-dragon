c2e2e3908d312c447a5377108ac492da572c78ab
mike-goodwin <mike.goodwin@owaps.org>
2016-05-10 22:22:10 +0100

    support use of local session store

--- td/config/session.config.js
+++ td/config/session.config.js
@@ -4,24 +4,31 @@ var session = require('express-session');
 var AzureTablesStoreFactory = require('connect-azuretables')(session);
 
 function configSessions(app) {
-    
+
     var cookieOptions = { maxAge: 3600000 };
     var logger = require('../config/loggers.config').logger;
-    
-    if(process.env.NODE_ENV != 'development' && process.env.NODE_ENV) {
+
+    if (process.env.NODE_ENV && process.env.NODE_ENV != 'development') {
         cookieOptions.secure = true;
     } else {
-        logger.error({security: true}, 'secure session cookie flag was false - should only happen in dev environments');
+        logger.error({ security: true }, 'secure session cookie flag was false - should only happen in dev environments');
     }
 
-    app.use(session({
-        store: AzureTablesStoreFactory.create({errorLogger: logger.error.bind(logger)}),
+    var sessionOptions = {
         secret: process.env.SESSION_SIGNING_KEY,
         resave: false,
         saveUninitialized: false,
         rolling: true,
         cookie: cookieOptions
-    }));
+    };
+
+    if (process.env.SESSION_STORE != 'local') {
+        sessionOptions.store = AzureTablesStoreFactory.create({ errorLogger: logger.error.bind(logger) });
+    } else {
+        logger.error({ security: true }, 'local session store used - should only happen in dev environments');
+    }
+
+    app.use(session(sessionOptions));
 }
 
 module.exports = configSessions;
