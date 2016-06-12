56a2a7c87b4f47f92d56b7c186e38ed2382bc72e
mike-goodwin <mike.goodwin@owaps.org>
2016-06-12 20:46:22 +0100

    added threat engine to core

--- src/services/index.js
+++ src/services/index.js
@@ -1,2 +1,3 @@
 var app = require('angular').module('app');
 app.factory('routemediator', ['$rootScope', '$location', 'config', 'logger', require('./routemediator')]);
+app.factory('threatengine', ['$q', require('./threatengine')]);
