6e7b880f618b8d8ff8bf2c220c8dd127ecaf877d
mike-goodwin <mike.goodwin@owaps.org>
2016-06-16 21:41:01 +0100

    logger unit tests passing

--- test/spec/logger_spec.js
+++ test/spec/logger_spec.js
@@ -1,9 +1,7 @@
 'use strict';
 
-var logger;
-var $rootScope;
-var $httpBackend;
-var mockLog;
+
+
 
 var logMessage = 'message';
 var logSource = 'source';
@@ -11,31 +9,17 @@ var logData = 'data';
 
 describe('logger service:', function () {
 
-    beforeEach(function () {
+    var mockLog = {};
+    //$log mocks
+    mockLog.log = function () { };
+    mockLog.error = function () { };
 
-        mockLog = {};
-
-        angular.mock.module('app');
-
-        angular.mock.module(function ($provide) {
-            $provide.value('$log', mockLog);
-        });
-
-        angular.mock.inject(function (_$rootScope_, _$httpBackend_, _logger_) {
-            logger = _logger_;
-            $rootScope = _$rootScope_;
-            $httpBackend = _$httpBackend_;
-            $httpBackend.expectGET().respond();
-        });
-
-        $rootScope.$apply();
-
-        //$log mocks
-        mockLog.log = function () { };
+    beforeEach(function() {
         spyOn(mockLog, 'log');
-        mockLog.error = function () { };
         spyOn(mockLog, 'error');
     });
+
+    var logger = require('../../src/common/logger')(mockLog);
 
     describe('log tests:', function () {
 
