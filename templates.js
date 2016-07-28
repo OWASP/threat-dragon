c58f27278e3fd4b2ee1b041092de992e2bd8344a
mike-goodwin <mike.goodwin@owaps.org>
2016-07-28 20:23:09 +0100

    Moved templates to core

--- templates.js
+++ templates.js
@@ -1,6 +1,113 @@
 angular.module('templates', [])
   .run(['$templateCache', function($templateCache) {
-    $templateCache.put('layout/shell.html',
+    $templateCache.put('diagrams/confirmReloadOnDirty.html',
+    '﻿<div>\n' +
+    '    <div class="modal-header">\n' +
+    '        <h3>Are you sure?</h3>\n' +
+    '    </div>\n' +
+    '    <div class="modal-body">\n' +
+    '        Your diagram has unsaved changes and if you reload they will be lost!\n' +
+    '        Press Cancel to keep the unsaved changes, or press OK to reload the diagram and lose the unsaved changes.\n' +
+    '    </div>\n' +
+    '    <div class="modal-footer">\n' +
+    '        <button class="btn btn-default" ng-click="onOK()">OK</button>\n' +
+    '        <button class="btn btn-primary" ng-click="onCancel()">Cancel</button>\n' +
+    '    </div>\n' +
+    '</div>\n' +
+    '')
+  $templateCache.put('diagrams/modalAccept.html',
+    '﻿<button class="btn btn-primary" ng-disabled="parameter.editing && (!threatEditForm.$dirty || !threatEditForm.$valid)" ng-click="onAction()">\n' +
+    '    Accept\n' +
+    '</button>')
+  $templateCache.put('diagrams/modalIgnore.html',
+    '﻿<button class="btn btn-default" ng-click="onAction()">\n' +
+    '    Ignore\n' +
+    '</button>')
+  $templateCache.put('diagrams/ThreatEditPane.html',
+    '﻿<div>\n' +
+    '    <div class="modal-header">\n' +
+    '        <h3>{{parameter.heading}}</h3>\n' +
+    '    </div>\n' +
+    '    <div class="modal-body">\n' +
+    '        <form name="threatEditForm">\n' +
+    '            <div class="form-group">\n' +
+    '                <label>Title</label>\n' +
+    '                <input name="titleInput" class="form-control" ng-required="true" type="text" ng-model="parameter.threat.title" placeholder="A short title for the threat" />\n' +
+    '                <div ng-show="!threatEditForm.titleInput.$valid && threatEditForm.titleInput.$dirty">\n' +
+    '                    <p>\n' +
+    '                        <div class="alert alert-danger" role="alert">\n' +
+    '                            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>\n' +
+    '                            <span class="sr-only">Error:</span>\n' +
+    '                            The threat title cannot be empty.\n' +
+    '                        </div>\n' +
+    '                    </p>\n' +
+    '                </div>\n' +
+    '            </div>\n' +
+    '            <div class="form-group">\n' +
+    '                <label>STRIDE threat type</label>\n' +
+    '                <select name="typeInput" class="form-control" ng-required="true" ng-model="parameter.threat.type">\n' +
+    '                    <option selected>Spoofing</option>\n' +
+    '                    <option>Tampering</option>\n' +
+    '                    <option>Repudiation</option>\n' +
+    '                    <option>Information disclosure</option>\n' +
+    '                    <option>Denial of service</option>\n' +
+    '                    <option>Elevation of privilege</option>\n' +
+    '                </select>\n' +
+    '                <div ng-show="!threatEditForm.typeInput.$valid && threatEditForm.typeInput.$dirty">\n' +
+    '                    <p>\n' +
+    '                        <div class="alert alert-danger" role="alert">\n' +
+    '                            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>\n' +
+    '                            <span class="sr-only">Error:</span>\n' +
+    '                            The threat type cannot be empty.\n' +
+    '                        </div>\n' +
+    '                    </p>\n' +
+    '                </div>\n' +
+    '            </div>\n' +
+    '            <div class="form-group pull-left">\n' +
+    '                <label>Threat status</label>\n' +
+    '                <div>\n' +
+    '                    <div class="btn-group" name="statusInput">\n' +
+    '                        <label class="btn btn-primary" name="statusInputOpen" ng-model="parameter.threat.status" uib-btn-radio="\'Open\'">Open</label>\n' +
+    '                        <label class="btn btn-primary" name="statusInputMitigated" ng-model="parameter.threat.status" uib-btn-radio="\'Mitigated\'">Mitigated</label>\n' +
+    '                    </div>\n' +
+    '                </div>\n' +
+    '            </div>\n' +
+    '            <div class="form-group pull-right">\n' +
+    '                <label>Severity</label>\n' +
+    '                <div>\n' +
+    '                    <div class="btn-group" required>\n' +
+    '                        <label class="btn btn-default" ng-model="parameter.threat.severity" uib-btn-radio="\'High\'">High</label>\n' +
+    '                        <label class="btn btn-default" ng-model="parameter.threat.severity" uib-btn-radio="\'Medium\'">Medium</label>\n' +
+    '                        <label class="btn btn-default" ng-model="parameter.threat.severity" uib-btn-radio="\'Low\'">Low</label>\n' +
+    '                    </div>\n' +
+    '                </div>\n' +
+    '            </div>\n' +
+    '            <div class="clearfix"></div>\n' +
+    '            <div class="form-group">\n' +
+    '                <label>Description</label>\n' +
+    '                <textarea name="descriptionInput" ng-model="parameter.threat.description" class="form-control" rows="5" placeholder="Detailed description of the threat"></textarea>\n' +
+    '            </div>\n' +
+    '            <div class="form-group">\n' +
+    '                <label>Mitigations</label>\n' +
+    '                <textarea name="mitigationInput" ng-model="parameter.threat.mitigation" class="form-control" rows="5" placeholder="Mitigations for the threat"></textarea>\n' +
+    '            </div>\n' +
+    '        </form>\n' +
+    '    </div>\n' +
+    '    <div ng-if="parameter.editing" class="modal-footer">\n' +
+    '        <button class="btn btn-primary" ng-disabled="!threatEditForm.$dirty || !threatEditForm.$valid" ng-click="onOK()">Save</button>\n' +
+    '        <button class="btn btn-default" ng-click="onCancel()">Cancel</button>\n' +
+    '    </div>\n' +
+    '    <div ng-if="!parameter.editing" class="modal-footer">\n' +
+    '        <span class="pull-left">\n' +
+    '            <input type="checkbox" ng-model="applyToAll">\n' +
+    '            Do this for all remaining threats \n' +
+    '        </span>\n' +
+    '        <tmt-modal-close action="onOK(applyToAll)" new-class="fade-left" template-url="diagrams/modalAccept.html"></tmt-modal-close>\n' +
+    '        <tmt-modal-close action="onCancel(applyToAll)" new-class="fade-down" template-url="diagrams/modalIgnore.html"></tmt-modal-close>\n' +
+    '    </div>\n' +
+    '</div>\n' +
+    '')
+  $templateCache.put('layout/shell.html',
     '﻿<div ng-controller="shell as vm">\n' +
     '    <header ng-if="!vm.suppressBanner" class="clearfix">\n' +
     '        <div ng-include="\'layout/topnav.html\'"></div>\n' +
@@ -69,6 +176,21 @@ angular.module('templates', [])
     '        </div>\n' +
     '    </div>\n' +
     '</nav>\n' +
+    '')
+  $templateCache.put('threatmodels/confirmReloadOnDirty.html',
+    '﻿<div>\n' +
+    '    <div class="modal-header">\n' +
+    '        <h3>Are you sure?</h3>\n' +
+    '    </div>\n' +
+    '    <div class="modal-body">\n' +
+    '        Your threat model has unsaved changes and if you reload they will be lost!\n' +
+    '        Press Cancel to keep the unsaved changes, or press OK to reload the threat model and lose the unsaved changes.\n' +
+    '    </div>\n' +
+    '    <div class="modal-footer">\n' +
+    '        <button class="btn btn-default" ng-click="onOK()">OK</button>\n' +
+    '        <button class="btn btn-primary" ng-click="onCancel()">Cancel</button>\n' +
+    '    </div>\n' +
+    '</div>\n' +
     '')
 
   }]);
