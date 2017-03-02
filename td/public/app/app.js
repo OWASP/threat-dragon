a86a11077f8996a156420caa599e27522a88b3e0
mike-goodwin <mike.goodwin@owaps.org>
2017-03-02 22:02:09 +0000

    browserification prep - vendor libs

--- td/public/app/app.js
+++ td/public/app/app.js
@@ -1,28 +1,32 @@
-﻿(function () {
-    'use strict';
-    
-    
-    //temporary fix for Chrome/Jointjs problem
-    SVGElement.prototype.getTransformToElement = SVGElement.prototype.getTransformToElement || function(toElement) {
-        return toElement.getScreenCTM().inverse().multiply(this.getScreenCTM());
-    };
+﻿'use strict';
 
-    var app = angular.module('app', ['ui.bootstrap', 'ngRoute', 'common', 'xeditable']);
+var angular = require('angular');
+require('angular-route');
+require('angular-xeditable');
+var $ = window.jQuery = require('jquery');
+require('bootstrap');
+require('angular-ui-bootstrap');
+require('angular-animate');
 
-    app.run(['$rootScope', '$location',
-    function ($rootScope, $location) { 
+//temporary fix for Chrome/Jointjs problem
+SVGElement.prototype.getTransformToElement = SVGElement.prototype.getTransformToElement || function (toElement) {
+    return toElement.getScreenCTM().inverse().multiply(this.getScreenCTM());
+};
+
+var app = angular.module('app', ['ui.bootstrap', 'ngRoute', 'common', 'xeditable']);
+
+app.run(['$rootScope', '$location',
+    function ($rootScope, $location) {
         $rootScope.location = $location;
     }]);
 
-    // Handle routing errors and success events
-    app.run(['$route', '$rootScope', 'routemediator',
+// Handle routing errors and success events
+app.run(['$route', '$rootScope', 'routemediator',
     function ($route, $rootScope, routemediator) {
         routemediator.setRoutingHandlers();
     }]);
-    
-    //config for angular-xeditable
-    app.run(function(editableOptions) {
-        editableOptions.theme = 'bs3';
-    });
 
-})();
+//config for angular-xeditable
+app.run(['editableOptions', function (editableOptions) {
+    editableOptions.theme = 'bs3';
+}]);
