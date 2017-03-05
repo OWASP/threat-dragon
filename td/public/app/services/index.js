246daa68656ca79ebaef4996a8898f39e672143d
mike-goodwin <mike.goodwin@owaps.org>
2017-03-05 09:24:15 +0000

    browserify threatengine

--- td/public/app/services/index.js
+++ td/public/app/services/index.js
@@ -1,2 +1,3 @@
 var app = require('angular').module('app');
 app.factory('datacontext', ['$q', '$http', 'common', require('./datacontext')]);
+app.factory('threatengine', [require('./threatengine')]);
