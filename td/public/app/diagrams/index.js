339649bb43fd6ed6efb98933be214cf4bbea67f1
Mike Goodwin <mike.goodwin@owasp.org>
2017-03-23 23:02:05 +0000

    diagram directives to core

--- td/public/app/diagrams/index.js
+++ td/public/app/diagrams/index.js
@@ -1,11 +1,8 @@
 var angular= require('angular');
 var app = angular.module('app');
-var diagramdirectives = require('./diagramdirectives');
 var elementPropertyDirectives = require('./elementpropdirectives');
 var diagram = require('./diagram');
 
-app.directive('tmtStencil', ['diagramming', diagramdirectives.stencil]);
-app.directive('tmtDiagram', ['common', 'diagramming', diagramdirectives.diagram]);
 app.directive('tmtModalClose', [elementPropertyDirectives.modalClose]);
 app.directive('tmtElementProperties', [elementPropertyDirectives.elementProperties]);
 app.directive('tmtElementThreats', ['$routeParams', '$location', 'common', 'dialogs', elementPropertyDirectives.elementThreats]);
