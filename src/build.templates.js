49f5c4a023c96fb0328c14bfbb7754062169909a
Mike Goodwin <mike.goodwin@owasp.org>
2017-03-25 14:31:04 +0000

    tidied templates

--- src/build.templates.js
+++
@@ -1,5 +0,0 @@
-require('./diagrams/ElementPropertiesPane.html');
-require('./diagrams/modalAccept.html');
-require('./diagrams/modalIgnore.html');
-require('./diagrams/ThreatEditPane.html');
-require('./diagrams/ThreatSummaryPane.html');
