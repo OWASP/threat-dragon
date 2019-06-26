b694752e8699ac416e2d98efb7d92e5faf32b493
Mike Goodwin <mike.goodwin@owasp.org>
2019-06-26 16:39:59 +0100

    tidied up stray code in unit tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,6 +1,2 @@
 var angular = require('angular');
-require('angular-mocks');
-require('angular-route');
-require('../../src/templates');
-var core = angular.module('owasp-threat-dragon-core', ['common', 'ui.bootstrap', 'ngRoute', 'templates']);
 require('../../src/index');
