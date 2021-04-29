5750690374030b218ce367a988fedd8228e5757d
lreading@TH.local <lreading@TH.local@thunderhead.com>
2021-04-29 15:36:43 -0400

    Refactored session config tests

--- td.server/test/config/session.config_spec.js
+++ td.server/test/config/session.config_spec.js
@@ -1,128 +1,69 @@
-'use strict';
+import AzureTablesStoreFactory from 'connect-azuretables';
+import { expect } from 'chai';
+import sinon from 'sinon';
 
-//this is a lot of mocking for small amount of test - am i doing something wrong?
-describe('session config tests', function() {
+import loggers from '../../src/config/loggers.config.js';
+import sessionConfig from '../../src/config/session.config.js';
 
-    var express = require('express');
-    var util = require('util');
-    var passport = require('passport');
-    var Strategy = require('passport-local').Strategy;
-    var app = express();
-    var request = require('supertest');
-    var finish_test = require('../supertest-jasmine');
-    var mockery = require('mockery');
-    var store;
-    var body = { username: 'mike', password: 'pwd' };
+describe('session config tests', () => {
+    const mockApp = {
+        use: () => {}
+    };
+    const sessionSigningKey = 'somekey';
 
-    //environment
-    var signingKey = 'signingKey';
-    var logger = require('../../src/config/loggers.config').logger;
-    process.env.SESSION_SIGNING_KEY = signingKey;
+    beforeEach(() => {
+        process.env.SESSION_SIGNING_KEY = sessionSigningKey;
+        process.env.SESSION_STORE = 'local';
+        sinon.spy(mockApp, 'use');
+        sinon.spy(loggers.logger, 'error');
+    });
 
-    beforeEach(function() {
+    afterEach(() => {
+        sinon.restore();
+    });
 
-        mockery.enable({ useCleanCache: true });
-        mockery.warnOnUnregistered(false);
-        mockery.warnOnReplace(false);
-        
-        //connect-azuretables mocks
-        mockery.registerMock('connect-azuretables', function(session) {
-
-            var MockStore = function() { };
-            util.inherits(MockStore, session.Store);
-
-            MockStore.prototype.set = function(sid, data, fn) {
-                return fn(null, true);
-            };
-
-            MockStore.prototype.get = function(sid, fn) {
-                return fn(null, sid);
-            };
-
-            var mockFactory = {
-                create: function() {
-                    store = new MockStore();
-                    return store;
-                }
-            };
-
-            return mockFactory;
+    describe('dev environment', () => {
+        beforeEach(() => {
+            sessionConfig(mockApp);
         });
 
-        passport.use(new Strategy(
-            function(username, password, cb) {
-                return cb(null, { name: username });
-            }));
-
-        passport.serializeUser(function(user, cb) {
-            cb(null, user);
+        it('logs an error about insecure cookies', () => {
+            expect(loggers.logger.error).to.have.been.calledWith(
+                { security: true },
+                sinon.match('cookie')
+            );
         });
 
-        passport.deserializeUser(function(obj, cb) {
-            cb(null, obj);
+        it('logs an error about local session storage', () => {
+            expect(loggers.logger.error).to.have.been.calledWith(
+                { security: true },
+                sinon.match('local session store used')
+            );
         });
 
-        spyOn(logger, 'error');
-        require('../../src/config/session.config')(app);
-        app.use(passport.initialize());
-        app.use(passport.session());
-        require('../../src/config/parsers.config')(app);
-        
-        jasmine.clock().install();
+        it('tells the app to use the express session', () => {
+            expect(mockApp.use).to.have.been.calledOnce;
+        });
     });
 
-    afterEach(function() {
-        mockery.disable();
-        jasmine.clock().uninstall();
-    });
+    describe('prod environment', () => {
+        beforeEach(() => {
+            process.env.NODE_ENV = 'simulated_production';
+            process.env.SESSION_STORE = 'azure';
+            sinon.spy(AzureTablesStoreFactory);
+        });
 
-    after(function() {
-        mockery.deregisterAll();
-    });
+        afterEach(() => {
+            process.env.NODE_ENV = 'development';
+        });
 
-    it('should set a session cookie', function(done) {
-        
-        var baseTime = new Date(1000);
-        var maxAge = 'Thu, 01 Jan 1970 01:00:01 GMT';
-        jasmine.clock().mockDate(baseTime);
+        it('sets the secure cookie option', () => {
+            expect(loggers.logger.error).not.to.have.been.called;
+        });
 
-        app.post('/',
-            passport.authenticate('local', { failureRedirect: '/error' }),
-            function(req, res) {
-                res.status(200);
-                res.send('result');
-            });
-
-        request(app)
-            .post('/')
-            .type('form')
-            .send(body)
-            .expect(200)
-            .expect(function(res) {
-                expect(res.headers['set-cookie'][0].indexOf('connect.sid') >= 0).toBe(true);
-                expect(res.headers['set-cookie'][0].indexOf('HttpOnly') >= 0).toBe(true);
-                expect(res.headers['set-cookie'][0].indexOf('Path=/') >= 0).toBe(true);
-                expect(res.headers['set-cookie'][0].indexOf(maxAge) >= 0).toBe(true);
-            })
-            .end(finish_test(done));
-    });
-
-    it('should set the user on the session', function(done) {
-
-        app.post('/',
-            passport.authenticate('local', { failureRedirect: '/error' }),
-            function(req, res) {
-                expect(req.session.user).toEqual(body.username);
-                res.status(200);
-                res.send('result');
-            });
-
-        request(app)
-            .post('/')
-            .type('form')
-            .send(body)
-            .expect(200)
-            .end(finish_test(done));
+        it('sets the session store to azure table storage', () => {
+            expect(loggers.logger.error).not.to.have.been.called;
+        });
     });
 });
 
