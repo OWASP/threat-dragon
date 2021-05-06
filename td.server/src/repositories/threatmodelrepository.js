3f0906c3cbc5acce2e2b38738213f80c84f8cb84
lreading@TH.local <lreading@TH.local@thunderhead.com>
2021-05-06 02:08:10 -0400

    More refactoring to ES6, fixing some unrelated depreciated code such as "new Buffer" calls.  Introducing async/await paradigm

--- td.server/src/repositories/threatmodelrepository.js
+++ td.server/src/repositories/threatmodelrepository.js
@@ -1,85 +1,65 @@
-'use strict';
-var github = require('octonode');
-var threatmodelrepository = {};
+import github from 'octonode';
 
-threatmodelrepository.repos = function (page, accessToken, cb) {
+const reposAsync = (page, accessToken) => github.client(accessToken).me().
+reposAsync(page);
 
-    var client = github.client(accessToken);
-    client.me().repos(page, cb);
+const branchesAsync = (repoInfo, accessToken) => {
+    const client = github.client(accessToken);
+    return client.repo(getRepoFullName(repoInfo)).branchesAsync(repoInfo.page);
 };
 
-threatmodelrepository.branches = function (repoInfo, accessToken, cb) {
+const modelsAsync = (branchInfo, accessToken) => github.client(accessToken).
+        repo(getRepoFullName(branchInfo)).
+        contentsAsync('ThreatDragonModels', branchInfo.branch);
 
-    var client = github.client(accessToken);
-    client.repo(getRepoFullName(repoInfo)).branches(repoInfo.page, cb);
+const modelAsync = (modelInfo, accessToken) => github.client(accessToken).
+        repo(getRepoFullName(modelInfo)).
+        contentsAsync(getModelPath(modelInfo), modelInfo.branch);
+
+const createAsync = (modelInfo, accessToken) => github.client(accessToken).
+        repo(getRepoFullName(modelInfo)).
+        createContentsAsync(
+            getModelPath(modelInfo),
+            'Created by OWASP Threat Dragon',
+            getModelContent(modelInfo),
+            modelInfo.branch
+        );
+
+const updateAsync = async (modelInfo, accessToken) => {
+    const content = await modelAsync(modelInfo, accessToken);
+    return github.client(accessToken).
+        repo(getRepoFullName(modelInfo)).
+        updateContentsAsync(
+            getModelPath(modelInfo),
+            'Updated by OWASP Threat Dragon',
+            getModelContent(modelInfo),
+            content[0].sha,
+            modelInfo.branch
+        );
 };
 
-threatmodelrepository.models = function (branchInfo, accessToken, cb) {
-
-    var client = github.client(accessToken);
-    client.repo(getRepoFullName(branchInfo)).contents('ThreatDragonModels', branchInfo.branch, cb);
+const deleteAsync = async (modelInfo, accessToken) => {
+    const content = await modelAsync(modelInfo, accessToken);
+    return github.client(accessToken).
+        repo(getRepoFullName(modelInfo)).
+        deleteContentsAsync(
+            getModelPath(modelInfo),
+            'Deleted by OWASP Threat Dragon',
+            content[0].sha,
+            modelInfo.branch
+        );
 };
 
-threatmodelrepository.model = function (modelInfo, accessToken, cb) {
+const getRepoFullName = (info) => `${info.organisation}/${info.repo}`;
+const getModelPath = (modelInfo) => `ThreatDragonModels/${modelInfo.model}/${modelInfo.model}.json`;
+const getModelContent = (modelInfo) => JSON.stringify(modelInfo.body, null, '  ');
 
-    var path = getModelPath(modelInfo);
-    var client = github.client(accessToken);
-    client.repo(getRepoFullName(modelInfo)).contents(path, modelInfo.branch, cb);
+export default {
+    branchesAsync,
+    createAsync,
+    deleteAsync,
+    modelAsync,
+    modelsAsync,
+    reposAsync,
+    updateAsync
 };
-
-threatmodelrepository.create = function (modelInfo, accessToken, cb) {
-
-    var path = getModelPath(modelInfo);
-    var client = github.client(accessToken);
-    var message = 'Created by OWASP Threat Dragon';
-    var content = getModelContent(modelInfo);
-    client.repo(getRepoFullName(modelInfo)).createContents(path, message, content, modelInfo.branch, cb);
-};
-
-threatmodelrepository.update = function (modelInfo, accessToken, cb) {
-
-    threatmodelrepository.model(modelInfo, accessToken, function (err, content) {
-
-        if (err) {
-            cb(err, null);
-        } else {
-            var path = getModelPath(modelInfo);
-            var client = github.client(accessToken);
-            var message = 'Updated by OWASP Threat Dragon';
-            var newContent = getModelContent(modelInfo);
-            client.repo(getRepoFullName(modelInfo)).updateContents(path, message, newContent, content.sha, modelInfo.branch, cb);
-        }
-    });
-};
-
-threatmodelrepository.deleteModel = function (modelInfo, accessToken, cb) {
-
-    threatmodelrepository.model(modelInfo, accessToken, function (err, content) {
-
-        if (err) {
-            cb(err, null);
-        } else {
-            var path = getModelPath(modelInfo);
-            var client = github.client(accessToken);
-            var message = 'Deleted by OWASP Threat Dragon';
-            client.repo(getRepoFullName(modelInfo)).deleteContents(path, message, content.sha, modelInfo.branch, cb);
-        }
-    });
-};
-
-
-//private functions
-
-function getRepoFullName(info) {
-    return info.organisation + '/' + info.repo;
-}
-
-function getModelPath(modelInfo) {
-    return 'ThreatDragonModels/' + modelInfo.model + '/' + modelInfo.model + '.json';
-}
-
-function getModelContent(modelInfo) {
-    return JSON.stringify(modelInfo.body, null, '  ');
-}
-
-module.exports = threatmodelrepository;
