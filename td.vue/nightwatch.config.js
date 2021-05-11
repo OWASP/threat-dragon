89a71b80d121d5793f8366dd8f399ba15af7cd24
lreading@TH.local <lreading@TH.local@thunderhead.com>
2021-05-11 15:45:52 -0400

    Updating nightwatch config to have sane defaults for chrome (window was too small, collapsed nav menu), changing action to test on both chrome and firefox again

--- td.vue/nightwatch.config.js
+++ td.vue/nightwatch.config.js
@@ -2,5 +2,14 @@ module.exports = {
     // Vue-cli adds a path by default
     // We do not have any custom assertions as of now
     // Tests will fail to start if we do not have anything in the path
-    custom_assertions_path: ''
+    custom_assertions_path: '',
+
+    desiredCapabilities: {
+        chromeOptions: {
+            args: [
+                "window-size=1280,800",
+                "--incognito"
+            ]
+        }
+    }
 };
