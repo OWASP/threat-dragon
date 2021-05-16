3434e113756dba30b1359b8c0d3e0c1555c0adac
Leo <leo.reading@gmail.com>
2021-05-16 00:03:20 -0400

    Fixing linting error, disabled the no-console rule for the healthcheck

--- td.server/src/healthcheck.js
+++ td.server/src/healthcheck.js
@@ -1,21 +1,14 @@
-console.log('LEO LEO LEO LEO');
-console.log('HEALTHCHECK STARTING!');
-
+/* eslint-disable no-console */
 const http = require('http');
 
-console.log('after require');
-
 http.get('http://localhost:3000/healthz', (res) => {
-    console.log('callback');
     const { statusCode } = res;
     
-    console.log(`status: ${statusCode}`);
     if (statusCode !== 200) {
         console.error(`Healthcheck failure: invalid status code: ${statusCode}`);
         process.exit(1);
     }
 
-    console.log('Exiting 0?');
     process.exit(0);
 }).on('error', (e) => {
     console.error(`Healthcheck failure: ${e.message}`);
