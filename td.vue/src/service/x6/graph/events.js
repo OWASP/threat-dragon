5c1534d252a23fc8229355e5658dd8e75b0723c0
Leo <leo.reading@gmail.com>
2021-05-23 00:18:09 -0400

    Adding more unit tests

--- td.vue/src/service/x6/graph/events.js
+++ td.vue/src/service/x6/graph/events.js
@@ -3,8 +3,6 @@
  * @description Event listeners for the graph
  */
 
-import trustBoundary from '../shapes/trust-boundary.js';
-
 // We need to add the router and connector data when a new edge is added.
 // https://x6.antv.vision/en/docs/tutorial/intermediate/events
 // If we do not, it is just a straight line with no collision detection
@@ -21,14 +19,6 @@ const removeCellTools = ({ cell }) => {
     }
 };
 
-const cellAdded = ({ cell }) => {
-    removeCellTools({ cell });
-    // Trust boundaries tend to need to be bigger than the size provided by the stencil
-    if (cell.constructor.name === trustBoundary.name) {
-        cell.resize(250, 500);
-    }
-};
-
 const mouseEnter = ({ cell }) => {
     const tools = [ 'boundary', 'button-remove' ];
     if (!cell.isNode()) {
@@ -42,7 +32,7 @@ const listen = (graph) => {
     graph.on('cell:mouseleave', removeCellTools);
     graph.on('cell:mouseenter', mouseEnter);
     graph.on('cell:changed:position', removeCellTools);
-    graph.on('cell:added', cellAdded);
+    graph.on('cell:added', removeCellTools);
     graph.on('cell:unselected', removeCellTools);
 };
 
---
+++ td.vue/tests/unit/service/x6/graph/events.spec.js
@@ -0,0 +1,122 @@
+import events from '@/service/x6/graph/events.js';
+
+describe('service/x6/graph/events.js', () => {
+    let cell, edge, graph;
+
+    beforeEach(() => {
+        graph = {
+            on: jest.fn()
+        };
+        cell = {
+            hasTools: jest.fn(),
+            removeTools: jest.fn(),
+            isNode: jest.fn(),
+            addTools: jest.fn()
+        };
+        edge = {};
+    });
+
+    describe('edge:connected', () => {
+        describe('new edge', () => {
+            beforeEach(() => {
+                graph.on.mockImplementation((evt, fn) => fn({ isNew: true, edge, cell }));
+                events.listen(graph);
+            });
+
+            it('listens to the event', () => {
+                expect(graph.on).toHaveBeenCalledWith('edge:connected', expect.any(Function));
+            });
+
+            it('adds the rounded connector to the edge', () => {
+                expect(edge.connector).toEqual('rounded');
+            });
+        });
+
+        describe('old edge', () => {
+            beforeEach(() => {
+                graph.on.mockImplementation((evt, fn) => fn({ isNew: false, edge, cell }));
+                events.listen(graph);
+            });
+
+            it('does not set the connector', () => {
+                expect(edge.connector).toBeUndefined();
+            });
+        });
+    });
+
+    describe('removeCellTools', () => {
+        describe('hasTools is true', () => {
+            beforeEach(() => {
+                cell.hasTools.mockImplementation(() => true);
+                graph.on.mockImplementation((evt, fn) => fn({ isNew: false, edge, cell }));
+                events.listen(graph);
+            });
+
+            it('calls removeTools', () => {
+                expect(cell.removeTools).toHaveBeenCalled();
+            });
+
+            it('listens to cell:mouseleave', () => {
+                expect(graph.on).toHaveBeenCalledWith('cell:mouseleave', expect.any(Function));
+            });
+
+            it('listens to cell:changed:position', () => {
+                expect(graph.on).toHaveBeenCalledWith('cell:changed:position', expect.any(Function));
+            });
+
+            it('listens to cell:added', () => {
+                expect(graph.on).toHaveBeenCalledWith('cell:added', expect.any(Function));
+            });
+
+            it('listens to cell:unselected', () => {
+                expect(graph.on).toHaveBeenCalledWith('cell:unselected', expect.any(Function));
+            });
+        });
+
+        describe('hasTools is false', () => {
+            beforeEach(() => {
+                cell.hasTools.mockImplementation(() => false);
+                graph.on.mockImplementation((evt, fn) => fn({ isNew: false, edge, cell }));
+                events.listen(graph);
+            });
+
+            it('calls removeTools', () => {
+                expect(cell.removeTools).not.toHaveBeenCalled();
+            });
+        });
+    });
+
+    describe('cell:mouseenter', () => {
+        describe('isNode is true', () => {
+            beforeEach(() => {
+                cell.isNode.mockImplementation(() => true);
+                graph.on.mockImplementation((evt, fn) => fn({ isNew: false, edge, cell }));
+                events.listen(graph);
+            });
+
+            it('listens to the mouseenter event', () => {
+                expect(graph.on).toHaveBeenCalledWith('cell:mouseenter', expect.any(Function));
+            });
+
+            it('adds the expected tools', () => {
+                expect(cell.addTools).toHaveBeenCalledWith(['boundary', 'button-remove']);
+            });
+        });
+
+        describe('isNode is false', () => {
+            beforeEach(() => {
+                cell.isNode.mockImplementation(() => false);
+                graph.on.mockImplementation((evt, fn) => fn({ isNew: false, edge, cell }));
+                events.listen(graph);
+            });
+
+            it('adds the expected tools', () => {
+                expect(cell.addTools).toHaveBeenCalledWith(['boundary', 'button-remove', 'vertices']);
+            });
+        });
+
+        describe('isNode is false', () => {
+
+        });
+    });
+});
