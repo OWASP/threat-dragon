106d60ace73b1d35c569182e65fc28b3a6881ffc
Jon Gadsden <jon.gadsden@forgerock.com>
2021-09-22 20:47:19 +0100

    pager directives renamed

--- td.desktop/core/layout/index.js
+++ td.desktop/core/layout/index.js
@@ -1,3 +1,3 @@
 var core = angular.module('tdCore');
-var directives = require('./directives');
-core.directive('tmtPager', [directives]);
+var directives = require('./pagerdirectives');
+core.directive('tdPager', [directives]);
