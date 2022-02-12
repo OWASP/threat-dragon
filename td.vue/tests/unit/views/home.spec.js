5c678258359510d0654401f6c0c44bf99f07de32
Leo <leo.reading@gmail.com>
2022-02-12 13:28:45 -0500

    Removing Github login button from electron version, only available on web version

--- td.vue/tests/unit/views/home.spec.js
+++ td.vue/tests/unit/views/home.spec.js
@@ -4,6 +4,7 @@ import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex';
 
 import { AUTH_SET_LOCAL } from '@/store/actions/auth.js';
+import env from '@/service/env.js';
 import Home from '@/views/Home.vue';
 import loginApi from '@/service/loginApi.js';
 import { PROVIDER_SELECTED } from '@/store/actions/provider.js';
@@ -15,64 +16,104 @@ describe('Home.vue', () => {
 
     let wrapper, localVue, mockStore;
 
-    beforeEach(() => {
-        localVue = createLocalVue();
-        localVue.use(Vuex);
-        localVue.use(BootstrapVue);
-        localVue.component('font-awesome-icon', FontAwesomeIcon);
-        mockStore = new Vuex.Store({
-            actions: {
-                [AUTH_SET_LOCAL]: () => {},
-                [PROVIDER_SELECTED]: () => {}
-            }
+    describe('browser', () => {
+        beforeEach(() => {
+            localVue = createLocalVue();
+            localVue.use(Vuex);
+            localVue.use(BootstrapVue);
+            localVue.component('font-awesome-icon', FontAwesomeIcon);
+            mockStore = new Vuex.Store({
+                actions: {
+                    [AUTH_SET_LOCAL]: () => {},
+                    [PROVIDER_SELECTED]: () => {}
+                }
+            });
+            jest.spyOn(loginApi, 'loginAsync').mockResolvedValue({ data: redirectUrl });
+            jest.spyOn(mockStore, 'dispatch');
+
+            // There may be a better way of doing this
+            // Source: https://remarkablemark.org/blog/2018/11/17/mock-window-location/
+            delete window.location;
+            window.location = { replace: jest.fn() };
+            router.push = jest.fn();
+
+            wrapper = shallowMount(Home, {
+                localVue,
+                store: mockStore,
+                mocks: {
+                    $t: key => key
+                }
+            });
         });
-        jest.spyOn(loginApi, 'loginAsync').mockResolvedValue({ data: redirectUrl });
-        jest.spyOn(mockStore, 'dispatch');
 
-        // There may be a better way of doing this
-        // Source: https://remarkablemark.org/blog/2018/11/17/mock-window-location/
-        delete window.location;
-        window.location = { replace: jest.fn() };
-        router.push = jest.fn();
+        describe('layout', () => {
 
-        wrapper = shallowMount(Home, {
-            localVue,
-            store: mockStore,
-            mocks: {
-                $t: key => key
-            }
+            it('renders the home view', () => {
+                expect(wrapper.exists()).toBe(true);
+            });
+        
+            it('has a b-container', () => {
+                expect(wrapper.findComponent(BContainer).exists()).toBe(true);
+            });
+        
+            it('has a jumbotron', () => {
+                expect(wrapper.findComponent(BJumbotron).exists()).toBe(true);
+            });
+        
+            it('displays the title', () => {
+                expect(wrapper.find('h1.display-3').text()).toContain('home.title');
+            });
+        
+            it('displays the threat dragon log', () => {
+                expect(wrapper.findComponent(BImg).attributes('src'))
+                    .toContain('threatdragon_logo_image');
+            });
+        
+            it('has the description of the project', () => {
+                expect(wrapper.find('p').exists()).toBe(true);
+            });
+        
+            it('has a login button', () => {
+                expect(wrapper.findComponent(TdProviderLoginButton).exists()).toEqual(true);
+            });
         });
     });
 
-    describe('layout', () => {
+    describe('electron', () => {
+        beforeEach(() => {
+            localVue = createLocalVue();
+            localVue.use(Vuex);
+            localVue.use(BootstrapVue);
+            localVue.component('font-awesome-icon', FontAwesomeIcon);
+            mockStore = new Vuex.Store({
+                actions: {
+                    [AUTH_SET_LOCAL]: () => {},
+                    [PROVIDER_SELECTED]: () => {}
+                }
+            });
+            env.isElectron = jest.fn().mockReturnValue(true);
+            jest.spyOn(loginApi, 'loginAsync').mockResolvedValue({ data: redirectUrl });
+            jest.spyOn(mockStore, 'dispatch');
 
-        it('renders the home view', () => {
-            expect(wrapper.exists()).toBe(true);
+            // There may be a better way of doing this
+            // Source: https://remarkablemark.org/blog/2018/11/17/mock-window-location/
+            delete window.location;
+            window.location = { replace: jest.fn() };
+            router.push = jest.fn();
+
+            wrapper = shallowMount(Home, {
+                localVue,
+                store: mockStore,
+                mocks: {
+                    $t: key => key
+                }
+            });
         });
-    
-        it('has a b-container', () => {
-            expect(wrapper.findComponent(BContainer).exists()).toBe(true);
-        });
-    
-        it('has a jumbotron', () => {
-            expect(wrapper.findComponent(BJumbotron).exists()).toBe(true);
-        });
-    
-        it('displays the title', () => {
-            expect(wrapper.find('h1.display-3').text()).toContain('home.title');
-        });
-    
-        it('displays the threat dragon log', () => {
-            expect(wrapper.findComponent(BImg).attributes('src'))
-                .toContain('threatdragon_logo_image');
-        });
-    
-        it('has the description of the project', () => {
-            expect(wrapper.find('p').exists()).toBe(true);
-        });
-    
-        it('has a login button', () => {
-            expect(wrapper.findComponent(TdProviderLoginButton).exists()).toEqual(true);
+
+        describe('layout', () => {
+            it('has only the local session button', () => {
+                expect(wrapper.findAllComponents(TdProviderLoginButton).length).toEqual(1);
+            });
         });
     });
 });
