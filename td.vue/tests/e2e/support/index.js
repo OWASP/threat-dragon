c13d05dea8ffbb92b8fc809d04b8f207f2d9b743
Jon Gadsden <jon.gadsden@forgerock.com>
2022-04-16 10:20:48 +0100

    update cypress and allow uncaught promises

--- td.vue/tests/e2e/support/index.js
+++ td.vue/tests/e2e/support/index.js
@@ -18,3 +18,14 @@ import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on('uncaught:exception', (err, runnable, promise) => {
+    // when the exception originated from an unhandled promise
+    // rejection, the promise is provided as a third argument
+    // you can turn off failing the test in this case
+    if (promise) {
+        return false
+    }
+    // we still want to ensure there are no other unexpected
+    // errors, so we let them fail the test
+})
