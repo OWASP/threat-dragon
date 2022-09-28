8ed6e3b415219711c48dcd7abf6643b5d5ea1bfd
jon gadsden <jon.gadsden@forgerock.com>
2022-09-28 19:18:31 +0100

    fix up tests for electron pull-down

--- td.vue/tests/unit/desktop/menu.spec.js
+++ td.vue/tests/unit/desktop/menu.spec.js
@@ -1,11 +1,11 @@
 // these unit tests are waiting to be filled in during the desktop development
-import menu from '@/desktop/desktop.menu.js';
+import menu from '@/desktop/menu.js';
 
 describe('desktop/desktop.menu.js', () => {
 
     it('returns a dropdown menu', () => {
         // the actual length of the menu system is platform dependent
-        expect(menu.menuTemplate.length).toBeGreaterThanOrEqual(5);
+        expect(menu.getMenuTemplate().length).toBeGreaterThanOrEqual(5);
     });
 
 });
