f83701a7522eccaa4db5840e5740757d0fae4b87
jon gadsden <jon.gadsden@forgerock.com>
2023-01-24 13:06:52 +0000

    only error server messages printed to electron console

--- td.vue/src/desktop/logger.js
+++ td.vue/src/desktop/logger.js
@@ -1,32 +1,31 @@
 'use strict';
 
+const logLevel = process.env.LOG_LEVEL || 'debug';
 const isDevelopment = process.env.NODE_ENV !== 'production';
 const isMacOS = process.platform === 'darwin';
 const isWin = (process.platform === 'win32' || process.platform === 'win64');
 
 export const log = require('electron-log');
+
 if (isDevelopment) {
     if (isMacOS) {
-        console.log('** Redirecting console log to ~/Library/Logs/Threat\\ Dragon/main.log');
+        console.log('** Redirecting console to log ~/Library/Logs/Threat\\ Dragon/main.log');
     } else if (isWin) {
-        console.log('** Redirecting console log to AppData\\Roaming\\Threat/ Dragon\\logs\\main.log');
+        console.log('** Redirecting console to log AppData\\Roaming\\Threat/ Dragon\\logs\\main.log');
     } else {
-        console.log('** Redirecting console log to ~/.config/Threat\\ Dragon/logs/main.log');
+        console.log('** Redirecting console to log ~/.config/Threat\\ Dragon/logs/main.log');
     }
 }
 
-// set up electron-specific logging
-const logLevel = process.env.LOG_LEVEL || 'debug';
+// set up electron-specific logLevel for the log file
 log.info('Electron log level is set to: ' + logLevel);
 log.transports.file.level = logLevel;
 
-// use electron-log instead of default console
+// redirect electron-console messages to electron-log file (also using logLevel)
 Object.assign(console, log.functions);
 
-if (!isDevelopment) {
-    // in production only print error messages to console
-    log.transports.console.level = 'error';
-}
+// only echo error messages to console
+log.transports.console.level = 'error';
 
 export default {
     log
