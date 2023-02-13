a7fe82e85af5d4826e8aeb9522bdb65ec49fa939
Christopher Hackett <ChristopherHackett@users.noreply.github.com>
2023-02-13 13:39:11 +0000

    Move class to branding span

--- td.vue/src/components/report/Coversheet.vue
+++ td.vue/src/components/report/Coversheet.vue
@@ -2,8 +2,8 @@
     <div class="no-print">
         <b-row class="mb-2 td-branding">
             <b-col>
-                <em class="td-brand-text">
-                    {{ new Date().toDateString() }} <span v-if="branding">| OWASP Threat Dragon </span>
+                <em>
+                    {{ new Date().toDateString() }} <span v-if="branding" class="td-brand-text">| OWASP Threat Dragon </span>
                 </em>
             </b-col>
         </b-row>
