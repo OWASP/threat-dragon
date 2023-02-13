eb515fe4812af1cbf8ba019fdb1c1b88b25f909d
Christopher Hackett <ChristopherHackett@users.noreply.github.com>
2023-02-13 11:33:26 +0000

    Apply branding toggle to just the branding

--- td.vue/src/components/report/Coversheet.vue
+++ td.vue/src/components/report/Coversheet.vue
@@ -2,8 +2,8 @@
     <div class="no-print">
         <b-row class="mb-2 td-branding">
             <b-col>
-                <em v-if="branding" class="td-brand-text">
-                    {{ new Date().toDateString() }} | OWASP Threat Dragon 
+                <em class="td-brand-text">
+                    {{ new Date().toDateString() }} <span v-if="branding">| OWASP Threat Dragon </span>
                 </em>
             </b-col>
         </b-row>
