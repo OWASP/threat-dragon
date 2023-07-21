dc49845b8e28f5709ce40191af64ee98b3c83b69
jon gadsden <jon.gadsden@forgerock.com>
2023-07-21 21:37:19 +0100

    add router for desktop

---
+++ td.vue/src/router/desktop.js
@@ -0,0 +1,36 @@
+import { providerTypes } from '../service/provider/providerTypes.js';
+
+const providerType = providerTypes.desktop;
+
+export const desktopRoutes = [
+    {
+        path: `/${providerType}/:threatmodel`,
+        name: `${providerType}ThreatModel`,
+        component: () => import(/* webpackChunkName: "threatmodel" */ '../views/ThreatModel.vue')
+    },
+    {
+        path: `/${providerType}/:threatmodel/edit`,
+        name: `${providerType}ThreatModelEdit`,
+        component: () => import(/* webpackChunkName: "threatmodel-edit" */ '../views/ThreatModelEdit.vue')
+    },
+    {
+        path: `/${providerType}/:threatmodel/edit/:diagram`,
+        name: `${providerType}DiagramEdit`,
+        component: () => import(/* webpackChunkName: "diagram-edit" */ '../views/DiagramEdit.vue')
+    },
+    {
+        path: `/${providerType}/threatmodel/import`,
+        name: `${providerType}ThreatModelImport`,
+        component: () => import(/* webpackChunkName: "threatmodel-import" */ '../views/ImportModel.vue')
+    },
+    {
+        path: `/${providerType}/threatmodel/new`,
+        name: `${providerType}NewThreatModel`,
+        component: () => import(/* webpackChunkName: "new-threatmodel" */ '../views/NewThreatModel.vue')
+    },
+    {
+        path: `/${providerType}/:threatmodel/report`,
+        name: `${providerType}Report`,
+        component: () => import(/* webpackChunkName: "report-model" */ '../views/ReportModel.vue')
+    }
+];
