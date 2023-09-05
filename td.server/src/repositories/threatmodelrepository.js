903ea74bca2dfc0d5ba38d73f60910089ebc4598
Zach Lanier <zach@n0where.org>
2023-09-05 09:03:07 -0400

    Add GitHub search option/feature
    This is somewhat of a rough PoC for enabling the use of GitHub repo search, rather than relying on a list of all repos accessible to the user. In the future, there might be a more user-friendly way to do this.
    
    Anyways, this adds two new GitHub-related env vars:
    - `GITHUB_USE_SEARCH`: if `true`, uses octonode's `search.reposAsync` instead of `reposAsync` passing in a search query
    - `GITHUB_SEARCH_QUERY`: specifies the search query to use when searching for repos for Threat Dragon to use
    
    A conditional check in `threatmodelcontroller` determines whether to use `reposAsync` _or_ `search.reposAsync` based on the above.
    
    Finally, this adds a wrapper function in `threatmodelrepository` to use octonode's `search.reposAsync`, passing in the aforementioned search query.

--- td.server/src/repositories/threatmodelrepository.js
+++ td.server/src/repositories/threatmodelrepository.js
@@ -19,6 +19,9 @@ const getClient = (accessToken) => {
 const reposAsync = (page, accessToken) => getClient(accessToken).me().
 reposAsync(page);
 
+const searchAsync = (accessToken, searchQuery) => getClient(accessToken).search()
+    .reposAsync({ q: searchQuery });
+
 const userAsync = async (accessToken) => {
     const resp = await getClient(accessToken).me().
 infoAsync();
@@ -87,6 +90,7 @@ export default {
     modelAsync,
     modelsAsync,
     reposAsync,
+    searchAsync,
     updateAsync,
     userAsync
 };
