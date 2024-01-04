3f1b6aa8ca25cd254b52d1fff5597e69b4f7a3e2
RAGHAV <94848646+raghav1030@users.noreply.github.com>
2024-01-05 02:51:13 +0530

    made language selection understandable

--- td.vue/src/components/LocaleSelect.vue
+++ td.vue/src/components/LocaleSelect.vue
@@ -5,7 +5,7 @@
         v-for="locale in $i18n.availableLocales"
         :key="`locale-${locale}`"
         :value="locale"
-        @click.native="updateLocale(locale)">{{ locale }}</b-dropdown-item>
+        @click.native="updateLocale(locale)">{{getLanguageName(locale)}}</b-dropdown-item>
     </b-dropdown>
   </div>
 </template>
@@ -34,7 +34,34 @@ export default {
                 // tell the electron main process that the locale has changed
                 window.electronAPI.updateMenu(locale);
             }
-        }
-    }
+        },
+        getLanguageName(locale) {
+            switch (locale) {
+                case 'ara':
+                return 'العربية'; // Arabic
+                case 'deu':
+                return 'Deutsch'; // German
+                case 'ell':
+                return 'Ελληνικά'; // Greek
+                case 'eng':
+                return 'English';
+                case 'spa':
+                return 'Español'; // Spanish
+                case 'fin':
+                return 'Suomi'; // Finnish
+                case 'fra':
+                return 'Français'; // French
+                case 'hin':
+                return 'हिंदी'; // Hindi
+                case 'por':
+                return 'Português'; // Portuguese
+                case 'zho':
+                return '中文'; // Chinese
+                default:
+                return locale; // Default to the original locale code if not found
+            }
+            }
+    },
+    
 };
 </script>
