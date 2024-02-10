d2619cd7c0dcec7088b2e40411d4b15254748f62
Steve Winter <steve.winter7@gmail.com>
2024-02-10 10:39:48 +1000

    Missed commit of Gitlab.js #9-Gitlab

---
+++ td.server/src/env/Gitlab.js
@@ -0,0 +1,27 @@
+import { Env } from './Env.js';
+
+class GitlabEnv extends Env {
+    constructor () {
+        super('Gitlab');
+    }
+
+    get prefix () {
+        return 'GITLAB_';
+    }
+
+    get properties () {
+        return [
+            { key: 'CLIENT_ID', required: false },
+            { key: 'CLIENT_SECRET', required: false },
+            { key: 'SCOPE', required: false },
+            { key: 'ENTERPRISE_HOSTNAME', required: false },
+            { key: 'ENTERPRISE_PROTOCOL', required: false },
+            { key: 'ENTERPRISE_PORT', required: false },
+            { key: 'USE_SEARCH', required: false },
+            { key: 'SEARCH_QUERY', required: false },
+            { key: 'REDIRECT_URI', required: false }
+        ];
+    }
+}
+
+export default GitlabEnv;
