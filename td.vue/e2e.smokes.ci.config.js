808e27d9f750398d86d76ea7a7be155394ed98ee
Jon Gadsden <jon.gadsden@owasp.org>
2024-04-16 20:41:49 +0100

    ensure e2e smokes tests run without error

--- td.vue/e2e.smokes.ci.config.js
+++ td.vue/e2e.smokes.ci.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
     screenshotsFolder: 'tests/e2e/screenshots',
     videosFolder: 'tests/e2e/videos',
     e2e: {
-        specPattern: [ 'tests/e2e/specs/home.cy.js', 'tests/e2e/specs/smokes/*.cy.js' ],
+        specPattern: [ 'tests/e2e/specs/smokes/*.cy.js' ],
         supportFile: 'tests/e2e/support/e2e.js',
         baseUrl: 'http://localhost:3000/'
     }
--- td.vue/tests/e2e/specs/threatmodel/edit.cy.js
+++ td.vue/tests/e2e/specs/threatmodel/edit.cy.js
@@ -1,78 +1,127 @@
-describe('editing a threat model', () => {
-    const newName = 'Something New Goes Here!';
-
-    beforeEach(() => {
-        cy.get('#local-login-btn').click();
-        cy.get('a[href="#/local/threatmodel/new"]').click();
-        cy.url().should('equal', Cypress.config().baseUrl + '#/local/New%20Threat%20Model/edit');
-    });
-
-    afterEach(() => {
-        cy.get('#nav-sign-out').click();
-    });
-
-    describe('cancel', () => {
-        it('goes back without prompting if data has not been edited', () => {
-            cy.get('#td-close-btn').click();
-            cy.url().should('equal', Cypress.config().baseUrl + '#/local/New%20Threat%20Model');
-            cy.contains('New Threat Model');
+describe('editing threat models', () => {
+    describe('editing a new model', () => {
+        beforeEach(() => {
+            cy.get('#local-login-btn').click();
+            cy.get('a[href="#/local/threatmodel/new"]').click();
+            cy.url().should('equal', Cypress.config().baseUrl + '#/local/New%20Threat%20Model/edit');
         });
 
-        describe('after editing', () => {
-            beforeEach(() => {
-                cy.get('#title').clear().type(newName);
-                cy.contains(`Editing: ${newName}`);
+        it('can edit the title', () => {
+            cy.get('#title').clear().type('new name');
+            cy.contains('Editing: new name');
+        });
+
+        it('can edit the owner', () => {
+            cy.get('#owner').should('be.visible')
+                .should('not.be', 'disabled')
+                .type('owner');
+        });
+
+        it('can edit the reviewer', () => {
+            cy.get('#reviewer').should('be.visible')
+                .should('not.be', 'disabled')
+                .type('reviewer');
+        });
+
+        it('can edit the description', () => {
+            cy.get('#description').should('be.visible')
+                .should('not.be', 'disabled')
+                .type('description');
+        });
+
+        it('should have the contributors field', () => {
+            cy.get('#contributors').should('be.visible');
+        });
+
+        it('can add a new diagram', () => {
+            cy.get('.add-diagram-link').click();
+            cy.get('#diagram-group-0').should('be.visible');
+            cy.get('.input-group-append').click();
+            cy.get('#diagram-group-0').should('not.be', 'visible');
+        });
+
+        it('has the form control buttons', () => {
+            cy.get('#td-save-btn').should('be.visible');
+            cy.get('#td-reload-btn').should('be.visible');
+            cy.get('#td-close-btn').should('be.visible');
+        });
+    });
+
+    describe('editing a new model', () => {
+        const newName = 'Something New Goes Here!';
+
+        beforeEach(() => {
+            cy.get('#local-login-btn').click();
+            cy.get('a[href="#/local/threatmodel/new"]').click();
+            cy.url().should('equal', Cypress.config().baseUrl + '#/local/New%20Threat%20Model/edit');
+        });
+
+        afterEach(() => {
+            cy.get('#nav-sign-out').click();
+        });
+
+        describe('cancel', () => {
+            it('goes back without prompting if data has not been edited', () => {
                 cy.get('#td-close-btn').click();
-            });
-
-            it('does not reset if the user selects cancel', () => {
-                cy.get('.modal-footer .btn-secondary').contains('Cancel').click();
-                cy.url().should('equal', Cypress.config().baseUrl + '#/local/New%20Threat%20Model/edit');
-                cy.contains(`Editing: ${newName}`);
-            });
-
-            it('resets data and goes back if the user selects ok', () => {
-                cy.get('.modal-footer .btn-danger').contains('OK').click();
                 cy.url().should('equal', Cypress.config().baseUrl + '#/local/New%20Threat%20Model');
                 cy.contains('New Threat Model');
             });
+
+            describe('after editing', () => {
+                beforeEach(() => {
+                    cy.get('#title').clear().type(newName);
+                    cy.contains(`Editing: ${newName}`);
+                    cy.get('#td-close-btn').click();
+                });
+
+                it('does not reset if the user selects cancel', () => {
+                    cy.get('.modal-footer .btn-secondary').contains('Cancel').click();
+                    cy.url().should('equal', Cypress.config().baseUrl + '#/local/New%20Threat%20Model/edit');
+                    cy.contains(`Editing: ${newName}`);
+                });
+
+                it('resets data and goes back if the user selects ok', () => {
+                    cy.get('.modal-footer .btn-danger').contains('OK').click();
+                    cy.url().should('equal', Cypress.config().baseUrl + '#/local/New%20Threat%20Model');
+                    cy.contains('New Threat Model');
+                });
+            });
         });
-    });
 
-    describe('reload', () => {
-        it('does nothing if data has not changed', () => {
-            cy.get('#td-reload-btn').click();
-            cy.contains(`Editing: New Threat Model`);
+        describe('reload', () => {
+            it('does nothing if data has not changed', () => {
+                cy.get('#td-reload-btn').click();
+                cy.contains(`Editing: New Threat Model`);
+            });
+
+            describe('with changed data', () => {
+                beforeEach(() => {
+                    cy.get('#title').clear().type(newName);
+                    cy.contains(`Editing: ${newName}`);
+                    cy.get('#td-reload-btn').click();
+                });
+
+                it('does not reload if the user cancels', () => {
+                    cy.get('.modal-footer .btn-secondary').contains('Cancel').click();
+                    cy.contains(`Editing: ${newName}`);
+                });
+
+                it('reloads if the user confirms', () => {
+                    cy.get('.modal-footer .btn-danger').contains('OK').click();
+                    cy.contains('Editing: New Threat Model');
+                });
+            });
         });
 
-        describe('with changed data', () => {
-
-            beforeEach(() => {
+        describe.skip('save', () => {
+            // TODO: Write better tests after implementation details of saving locally are determined
+            it('returns to the threat model view', () => {
                 cy.get('#title').clear().type(newName);
                 cy.contains(`Editing: ${newName}`);
-                cy.get('#td-reload-btn').click();
+                cy.get('#td-save-btn').click();
+                cy.url().should('equal', Cypress.config().baseUrl + '#/local/New%20Threat%20Model');
+                cy.contains(newName);
             });
-
-            it('does not reload if the user cancels', () => {
-                cy.get('.modal-footer .btn-secondary').contains('Cancel').click();
-                cy.contains(`Editing: ${newName}`);
-            });
-
-            it('reloads if the user confirms', () => {
-                cy.get('.modal-footer .btn-danger').contains('OK').click();
-                cy.contains('Editing: New Threat Model');
-            });
-        });
-    });
-
-    describe.skip('save', () => {
-        // TODO: Write better tests after implementation details of saving locally are determined
-        it('returns to the threat model view', () => {
-            cy.get('#title').clear().type(newName);
-            cy.contains(`Editing: ${newName}`);
-            cy.get('#td-save-btn').click();
-            cy.url().should('equal', Cypress.config().baseUrl + '#/local/New%20Threat%20Model');
-            cy.contains(newName);
         });
     });
 });
