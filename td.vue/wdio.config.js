bf87bcf733063c71e7a52d0b51663550698d4444
Jon Gadsden <jon.gadsden@owasp.org>
2024-04-18 12:18:23 +0100

    update versions for desktop e2e tests

--- td.vue/wdio.config.js
+++ td.vue/wdio.config.js
@@ -3,6 +3,13 @@ const { join } = require('path');
 process.env.IS_TEST = true;
 
 const config = {
+    runner: 'local',
+    reporters: ['spec','json'],
+    specs: [ './tests/e2e/desktop/*.spec.js' ],
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+    maxInstances: 10,
     services: [
         [
             'electron',
@@ -21,18 +28,21 @@ const config = {
         'goog:chromeOptions': {
             // run chrome headless
             args: ['--headless', '--disable-gpu']
+        },
+        'wdio:electronServiceOptions': {
+            // custom application args
+            appArgs: []
         }
     }],
+    logLevel: 'debug',
+    bail: 0,
     hostname: 'localhost',
     port: 9519,
     waitforTimeout: 30000,
     connectionRetryCount: 10,
     connectionRetryTimeout: 30000,
-    logLevel: 'debug',
-    runner: 'local',
     // to redirect logs to files instead of console:
     outputDir: 'wdio-logs',
-    specs: ['./tests/e2e/desktop/*.spec.js'],
     framework: 'mocha',
     mochaOpts: {
         ui: 'bdd',
