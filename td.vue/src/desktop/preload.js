6a61ebca021ad65a99167dc87143c69c0f6a7067
Shubham-Patel07 <shubham2107patel@gmail.com>
2025-01-18 16:55:19 +0530

    Add: added preload script

---
+++ td.vue/src/desktop/preload.js
@@ -0,0 +1,7 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('darkMode', {
+    toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
+    system: () => ipcRenderer.invoke('dark-mode:system'),
+    get: () => ipcRenderer.invoke('dark-mode:get')
+});
