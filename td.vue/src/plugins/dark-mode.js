77ebfb94d57b3edda83a3bf49af5d7287daf6197
Shubham-Patel07 <shubham2107patel@gmail.com>
2025-01-27 15:50:16 +0530

    Add: Added few styles and resolved svg toggling

--- td.vue/src/plugins/dark-mode.js
+++ td.vue/src/plugins/dark-mode.js
@@ -2,6 +2,7 @@ export async function toggleDarkMode() {
     if (window.darkMode) {
         const isDark = await window.darkMode.toggle();
         document.body.className = isDark ? 'dark' : 'light';
+        console.log('Darkmode status: ' + isDark);
         return isDark;
     } else {
         console.error('Dark mode API is not available.');
@@ -14,6 +15,7 @@ export async function setSystemTheme() {
         await window.darkMode.system();
         const theme = await window.darkMode.get();
         document.body.className = theme.shouldUseDarkColors ? 'dark' : 'light';
+        console.log('Darkmode status: ' + theme.shouldUseDarkColors);
         return theme;
     } else {
         console.error('Dark mode API is not available.');
