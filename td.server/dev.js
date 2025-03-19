7682c469cb0bb168a9d13706a153ef247f9cf3fc
Eric Fitzgerald <github@efitz.net>
2025-03-19 03:08:15 -0400

    Fixing error message trying to access server.address during server startup.

--- td.server/dev.js
+++ td.server/dev.js
@@ -3,7 +3,12 @@ var appFactory = require('./src/app.js');
 var app = appFactory.default.create();
 
 var server = app.listen(app.get('port'), function() {
-    console.log('Development server listening at ' + server.address().address + ' on port ' +  server.address().port);
+    const address = server.address();
+    if (address) {
+        console.log('Development server listening at ' + address.address + ' on port ' + address.port);
+    } else {
+        console.log('Development server listening on port ' + app.get('port'));
+    }
 });
 
 process.once('SIGUSR2', 
