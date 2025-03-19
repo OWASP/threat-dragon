d179f5e607024cb8e74bfc45e9c1b5733a379867
Eric Fitzgerald <github@efitz.net>
2025-03-19 00:05:45 -0400

    added debug logging to troubleshoot google provider configuration

--- td.server/src/controllers/configcontroller.js
+++ td.server/src/controllers/configcontroller.js
@@ -6,13 +6,18 @@ const logger = loggerHelper.get('controllers/configcontroller.js');
 
 const config = (req, res) => responseWrapper.sendResponse(() => getConfig(), req, res, logger);
 
-export const getConfig = () => ({
-    bitbucketEnabled: env.get().config.BITBUCKET_CLIENT_ID !== undefined && env.get().config.BITBUCKET_CLIENT_ID !== null,
-    githubEnabled: env.get().config.GITHUB_CLIENT_ID !== undefined && env.get().config.GITHUB_CLIENT_ID !== null,
-    gitlabEnabled: env.get().config.GITLAB_CLIENT_ID !== undefined && env.get().config.GITLAB_CLIENT_ID !== null,
-    googleEnabled: env.get().config.VUE_APP_GOOGLE_CLIENT_ID !== undefined && env.get().config.VUE_APP_GOOGLE_CLIENT_ID !== null,
-    localEnabled: true,
-});
+export const getConfig = () => {
+    console.log('VUE_APP_GOOGLE_CLIENT_ID from env:', env.get().config.VUE_APP_GOOGLE_CLIENT_ID);
+    console.log('GOOGLE_CLIENT_ID from env:', env.get().config.GOOGLE_CLIENT_ID);
+    
+    return {
+        bitbucketEnabled: env.get().config.BITBUCKET_CLIENT_ID !== undefined && env.get().config.BITBUCKET_CLIENT_ID !== null,
+        githubEnabled: env.get().config.GITHUB_CLIENT_ID !== undefined && env.get().config.GITHUB_CLIENT_ID !== null,
+        gitlabEnabled: env.get().config.GITLAB_CLIENT_ID !== undefined && env.get().config.GITLAB_CLIENT_ID !== null,
+        googleEnabled: env.get().config.VUE_APP_GOOGLE_CLIENT_ID !== undefined && env.get().config.VUE_APP_GOOGLE_CLIENT_ID !== null,
+        localEnabled: true,
+    };
+};
 
 export default {
     config
