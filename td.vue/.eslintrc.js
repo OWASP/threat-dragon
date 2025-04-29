3dcb50fc5b4ee609fbf8ff674b9598071631bddd
Eric Fitzgerald <github@efitz.net>
2025-04-28 22:38:48 -0400

    Fixing lint issues with eslint vue plugin in td.vue

---
+++ td.vue/.eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+    // Create a standalone configuration
+    root: true,
+    env: {
+        node: true,
+        browser: true,
+        es2022: true,
+        jest: true
+    },
+    plugins: ['vue'],
+    extends: ['eslint:recommended', 'plugin:vue/essential'],
+    globals: {
+        __static: 'readonly',
+        vi: 'readonly',
+        google: 'readonly',
+        gapi: 'readonly',
+        before: 'readonly',
+        after: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        Component: 'readonly'
+    },
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module'
+    },
+    rules: {
+        // Common rules
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-unused-vars': ['warn', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
+        'no-undef': 'warn',
+        semi: ['error', 'always'],
+        quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+        indent: ['warn', 4],
+        'no-mixed-spaces-and-tabs': ['warn', 'smart-tabs'],
+        'linebreak-style': ['warn', 'unix'],
+        
+        // Vue specific rules
+        'vue/no-unused-components': 'warn',
+        'vue/html-indent': ['error', 4],
+        'vue/max-attributes-per-line': [
+            'warn',
+            {
+                singleline: {
+                    max: 3
+                },
+                multiline: {
+                    max: 1
+                }
+            }
+        ]
+    }
+};
