c398a9ff0ccf8c15d19b989b7b0a8d1e4c95171b
Jon Gadsden <jon.gadsden@owasp.org>
2025-11-21 10:55:25 +0000

    remove incorrect parameters to auto-update

--- td.vue/src/desktop/desktop.js
+++ td.vue/src/desktop/desktop.js
@@ -113,9 +113,6 @@ app.on('ready', async () => {
     createWindow();
 
     // check for updates from github releases site
-    autoUpdater.autoInstallOnAppQuit = true;
-    // require user to agree to download
-    autoUpdater.autoDownload = false;
     autoUpdater.checkForUpdatesAndNotify();
 });
 
