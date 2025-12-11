04e83226e969578c9b84a919ed2a7e83f8c9c8a5
gasparlamas <gasparlamas32@gmail.com>
2025-12-11 15:31:22 -0300

    Visual only feature done

--- td.vue/src/components/ThreatEditDialog.vue
+++ td.vue/src/components/ThreatEditDialog.vue
@@ -16,7 +16,8 @@
                         <b-form-group
                             id="title-group"
                             :label="$t('threats.properties.title')"
-                            label-for="title">
+                            label-for="title"
+                        >
                             <b-form-input
                                 id="title"
                                 v-model="threat.title"
@@ -26,29 +27,61 @@
                         </b-form-group>
                     </b-col>
                 </b-form-row>
-
+                <!-- BEGINNING ROW BELU - CATEGORY AND CARD-->
                 <b-form-row>
                     <b-col>
                         <b-form-group
                             id="threat-type-group"
                             :label="$t('threats.properties.type')"
-                            label-for="threat-type">
+                            label-for="threat-type"
+                        >
                             <b-form-select
                                 id="threat-type"
                                 v-model="threat.type"
-                                :options="threatTypes">
+                                :options="threatTypes"
+                            >
                             </b-form-select>
                         </b-form-group>
                     </b-col>
                 </b-form-row>
+                <!-- END ROW BELU - CATEGORY AND CARD-->
 
+                <!-- BEGINNING ROW GASPI - INFO CARD AND LINK TO THE CARD-->
+                <!-- Redirect only available on EoP (for now on Stride)-->
+                <b-form-row
+                    v-if="threat && threat.modelType === 'STRIDE'"
+                    style="margin-bottom: 16px"
+                >
+                    <b-col>
+                        <!-- Text is a link to the card (owasp for now) -->
+                        <a
+                            href="https://owasp.org/Top10/2021/es/index.html"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title="View card details"
+                            style="
+                                font-size: 16px;
+                                font-weight: normal;
+                                color: red;
+                                
+                                padding: 6px 10px;
+                                border-radius: 4px;
+                                display: inline-block;
+                            "
+                        >
+                            Card details: Data Validation & Encoding 9
+                        </a>
+                    </b-col>
+                </b-form-row>
+                <!-- END ROW GASPI - CARD AND LINK TO THE CARD-->
                 <b-form-row>
-                    <b-col md=5>
+                    <b-col md="5">
                         <b-form-group
                             id="status-group"
                             class="float-left"
                             :label="$t('threats.properties.status')"
-                            label-for="status">
+                            label-for="status"
+                        >
                             <b-form-radio-group
                                 id="status"
                                 v-model="threat.status"
@@ -58,11 +91,12 @@
                         </b-form-group>
                     </b-col>
 
-                    <b-col md=2>
+                    <b-col md="2">
                         <b-form-group
                             id="score-group"
                             :label="$t('threats.properties.score')"
-                            label-for="score">
+                            label-for="score"
+                        >
                             <b-form-input
                                 id="score"
                                 v-model="threat.score"
@@ -71,12 +105,13 @@
                         </b-form-group>
                     </b-col>
 
-                    <b-col md=5>
+                    <b-col md="5">
                         <b-form-group
                             id="severity-group"
                             class="float-right"
                             :label="$t('threats.properties.severity')"
-                            label-for="severity">
+                            label-for="severity"
+                        >
                             <b-form-radio-group
                                 id="severity"
                                 v-model="threat.severity"
@@ -92,11 +127,13 @@
                         <b-form-group
                             id="description-group"
                             :label="$t('threats.properties.description')"
-                            label-for="description">
+                            label-for="description"
+                        >
                             <b-form-textarea
                                 id="description"
                                 v-model="threat.description"
-                                rows="5">
+                                rows="5"
+                            >
                             </b-form-textarea>
                         </b-form-group>
                     </b-col>
@@ -107,11 +144,13 @@
                         <b-form-group
                             id="mitigation-group"
                             :label="$t('threats.properties.mitigation')"
-                            label-for="mitigation">
+                            label-for="mitigation"
+                        >
                             <b-form-textarea
                                 id="mitigation"
                                 v-model="threat.mitigation"
-                                rows="5">
+                                rows="5"
+                            >
                             </b-form-textarea>
                         </b-form-group>
                     </b-col>
@@ -120,37 +159,37 @@
 
             <template #modal-footer>
                 <div class="w-100">
-                <b-button
-                    v-if="!newThreat"
-                    variant="danger"
-                    class="float-left"
-                    @click="confirmDelete()"
-                >
-                    {{ $t('forms.delete') }}
-                </b-button>
-                <b-button
-                    v-if="newThreat"
-                    variant="danger"
-                    class="float-left"
-                    @click="immediateDelete()"
-                >
-                    {{ $t('forms.remove') }}
-                </b-button>
-                 <b-button
-                    variant="secondary"
-                    class="float-right"
-                    @click="updateThreat()"
-                >
-                    {{ $t('forms.apply') }}
-                </b-button>
-                <b-button
-                v-if="!newThreat"
-                variant="secondary"
-                class="float-right mr-2"
-                @click="hideModal()"
-            >
-                {{ $t('forms.cancel') }}
-            </b-button>
+                    <b-button
+                        v-if="!newThreat"
+                        variant="danger"
+                        class="float-left"
+                        @click="confirmDelete()"
+                    >
+                        {{ $t("forms.delete") }}
+                    </b-button>
+                    <b-button
+                        v-if="newThreat"
+                        variant="danger"
+                        class="float-left"
+                        @click="immediateDelete()"
+                    >
+                        {{ $t("forms.remove") }}
+                    </b-button>
+                    <b-button
+                        variant="secondary"
+                        class="float-right"
+                        @click="updateThreat()"
+                    >
+                        {{ $t("forms.apply") }}
+                    </b-button>
+                    <b-button
+                        v-if="!newThreat"
+                        variant="secondary"
+                        class="float-right mr-2"
+                        @click="hideModal()"
+                    >
+                        {{ $t("forms.cancel") }}
+                    </b-button>
                 </div>
             </template>
         </b-modal>
@@ -158,19 +197,19 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState } from "vuex";
 
-import { CELL_DATA_UPDATED } from '@/store/actions/cell.js';
-import tmActions from '@/store/actions/threatmodel.js';
-import dataChanged from '@/service/x6/graph/data-changed.js';
-import threatModels from '@/service/threats/models/index.js';
+import { CELL_DATA_UPDATED } from "@/store/actions/cell.js";
+import tmActions from "@/store/actions/threatmodel.js";
+import dataChanged from "@/service/x6/graph/data-changed.js";
+import threatModels from "@/service/threats/models/index.js";
 
 export default {
-    name: 'TdThreatEditDialog',
+    name: "TdThreatEditDialog",
     computed: {
         ...mapState({
             cellRef: (state) => state.cell.ref,
-            threatTop: (state) => state.threatmodel.data.detail.threatTop
+            threatTop: (state) => state.threatmodel.data.detail.threatTop,
         }),
         threatTypes() {
             if (!this.cellRef || !this.threat || !this.threat.modelType) {
@@ -178,70 +217,89 @@ export default {
             }
 
             const res = [];
-            const threatTypes = threatModels.getThreatTypesByElement(this.threat.modelType, this.cellRef.data.type);
+            const threatTypes = threatModels.getThreatTypesByElement(
+                this.threat.modelType,
+                this.cellRef.data.type
+            );
             Object.keys(threatTypes).forEach((type) => {
                 res.push(this.$t(type));
             }, this);
-            if(!res.includes(this.threat.type))
-                res.push(this.threat.type);
+            if (!res.includes(this.threat.type)) res.push(this.threat.type);
             return res;
         },
         statuses() {
             return [
-                { value: 'NotApplicable', text: this.$t('threats.status.notApplicable') },
-                { value: 'Open', text: this.$t('threats.status.open') },
-                { value: 'Mitigated', text: this.$t('threats.status.mitigated') }
+                {
+                    value: "NotApplicable",
+                    text: this.$t("threats.status.notApplicable"),
+                },
+                { value: "Open", text: this.$t("threats.status.open") },
+                {
+                    value: "Mitigated",
+                    text: this.$t("threats.status.mitigated"),
+                },
             ];
         },
         priorities() {
             return [
-                { value: 'TBD', text: this.$t('threats.severity.tbd') },
-                { value: 'Low', text: this.$t('threats.severity.low') },
-                { value: 'Medium', text: this.$t('threats.severity.medium') },
-                { value: 'High', text: this.$t('threats.severity.high') },
-                { value: 'Critical', text: this.$t('threats.severity.critical') }
+                { value: "TBD", text: this.$t("threats.severity.tbd") },
+                { value: "Low", text: this.$t("threats.severity.low") },
+                { value: "Medium", text: this.$t("threats.severity.medium") },
+                { value: "High", text: this.$t("threats.severity.high") },
+                {
+                    value: "Critical",
+                    text: this.$t("threats.severity.critical"),
+                },
             ];
         },
-        modalTitle() { return this.$t('threats.edit') + ' #' + this.number; }
+        modalTitle() {
+            return this.$t("threats.edit") + " #" + this.number;
+        },
     },
     data() {
         return {
             threat: {},
-            modelTypes: [
-                'CIA',
-                'CIADIE',
-                'LINDDUN',
-                'PLOT4ai',
-                'STRIDE'
-            ],
-            number: 0
+            modelTypes: ["CIA", "CIADIE", "LINDDUN", "PLOT4ai", "STRIDE"],
+            number: 0,
         };
     },
     methods: {
-        editThreat(threatId,state) {
-            const crnthreat = this.cellRef.data.threats.find(x => x.id === threatId);
-            this.threat = {...crnthreat};
+        editThreat(threatId, state) {
+            const crnthreat = this.cellRef.data.threats.find(
+                (x) => x.id === threatId
+            );
+            this.threat = { ...crnthreat };
             if (!this.threat) {
                 // this should never happen with a valid threatId
-                console.warn('Trying to access a non-existent threatId: ' + threatId);
+                console.warn(
+                    "Trying to access a non-existent threatId: " + threatId
+                );
             } else {
                 this.number = this.threat.number;
-                this.newThreat = state==='new';
+                this.newThreat = state === "new";
                 this.$refs.editModal.show();
             }
         },
         updateThreat() {
-            const threatRef = this.cellRef.data.threats.find(x => x.id === this.threat.id);
+            const threatRef = this.cellRef.data.threats.find(
+                (x) => x.id === this.threat.id
+            );
             if (threatRef) {
                 const objRef = this.cellRef.data;
-                if(!objRef.threatFrequency){
-                    const tmpfreq = threatModels.getFrequencyMapByElement(this.threat.modelType,this.cellRef.data.type);
-                    if(tmpfreq!==null)
-                        objRef.threatFrequency = tmpfreq;
+                if (!objRef.threatFrequency) {
+                    const tmpfreq = threatModels.getFrequencyMapByElement(
+                        this.threat.modelType,
+                        this.cellRef.data.type
+                    );
+                    if (tmpfreq !== null) objRef.threatFrequency = tmpfreq;
                 }
-                if(objRef.threatFrequency){
-                    Object.keys(objRef.threatFrequency).forEach((k)=>{
-                        if(this.$t(`threats.model.${this.threat.modelType.toLowerCase()}.${k}`)===this.threat.type)
+                if (objRef.threatFrequency) {
+                    Object.keys(objRef.threatFrequency).forEach((k) => {
+                        if (
+                            this.$t(
+                                `threats.model.${this.threat.modelType.toLowerCase()}.${k}`
+                            ) === this.threat.type
+                        )
                             objRef.threatFrequency[k]++;
                     });
                 }
@@ -262,15 +320,22 @@ export default {
             this.hideModal();
         },
         deleteThreat() {
-            if(!this.threat.new){
+            if (!this.threat.new) {
                 const threatMap = this.cellRef.data.threatFrequency;
-                Object.keys(threatMap).forEach((k)=>{
-                    if(this.$t(`threats.model.${this.threat.modelType.toLowerCase()}.${k}`)===this.threat.type)
+                Object.keys(threatMap).forEach((k) => {
+                    if (
+                        this.$t(
+                            `threats.model.${this.threat.modelType.toLowerCase()}.${k}`
+                        ) === this.threat.type
+                    )
                         threatMap[k]--;
                 });
             }
-            this.cellRef.data.threats = this.cellRef.data.threats.filter(x => x.id !== this.threat.id);
-            this.cellRef.data.hasOpenThreats = this.cellRef.data.threats.length > 0;
+            this.cellRef.data.threats = this.cellRef.data.threats.filter(
+                (x) => x.id !== this.threat.id
+            );
+            this.cellRef.data.hasOpenThreats =
+                this.cellRef.data.threats.length > 0;
             this.$store.dispatch(CELL_DATA_UPDATED, this.cellRef.data);
             this.$store.dispatch(tmActions.modified);
             dataChanged.updateStyleAttrs(this.cellRef);
@@ -279,14 +344,19 @@ export default {
             this.$refs.editModal.hide();
         },
         async confirmDelete() {
-            const confirmed = await this.$bvModal.msgBoxConfirm(this.$t('threats.confirmDeleteMessage'), {
-                title: this.$t('threats.confirmDeleteTitle'),
-                okTitle: this.$t('forms.delete'),
-                cancelTitle: this.$t('forms.cancel'),
-                okVariant: 'danger'
-            });
+            const confirmed = await this.$bvModal.msgBoxConfirm(
+                this.$t("threats.confirmDeleteMessage"),
+                {
+                    title: this.$t("threats.confirmDeleteTitle"),
+                    okTitle: this.$t("forms.delete"),
+                    cancelTitle: this.$t("forms.cancel"),
+                    okVariant: "danger",
+                }
+            );
 
-            if (!confirmed) { return; }
+            if (!confirmed) {
+                return;
+            }
 
             this.deleteThreat();
             this.hideModal();
@@ -294,8 +364,7 @@ export default {
         async immediateDelete() {
             this.deleteThreat();
             this.hideModal();
-        }
-    }
+        },
+    },
 };
-
 </script>
