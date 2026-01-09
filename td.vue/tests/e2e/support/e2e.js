ac004aea67469757f876786ea8892c2687b7e07b
André R. Ferreira <186318329+AndreRFerreira@users.noreply.github.com>
2026-01-09 21:47:22 +0100

    test(e2e): refactor support and add startup command

--- td.vue/tests/e2e/support/e2e.js
+++ td.vue/tests/e2e/support/e2e.js
@@ -1,10 +1,3 @@
 // support file used by cypress to provide global scripts
 
-// run before every test in every spec file, invokes the application 
-beforeEach(() => {
-    cy.log('Invoke the Threat Dragon application');
-    cy.visit('/', {
-        onBeforeLoad: (win) => win.sessionStorage.clear()
-    });
-    cy.get('#local-login-btn').should('be.visible');
-});
+import './commands/startup';
