1b4957ca8637a775f741c8924331f7687cb87d43
Leo Reading <leo.reading@gmail.com>
2026-06-04 23:24:38 -0400

    rename reverseproxy middleware to decodeRepoPaths

--- td.server/test/config/reverseproxy.config.spec.js
+++ td.server/test/config/reverseproxy.config.spec.js
@@ -2,9 +2,9 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 
 import { getMockRequest, getMockResponse } from '../mocks/express.mocks.js';
-import reverseproxy from '../../src/config/reverseproxy.config.js';
+import decodeRepoPaths from '../../src/config/decodeRepoPaths.config.js';
 
-describe('config/reverseproxy.config.js', () => {
+describe('config/decodeRepoPaths.config.js', () => {
     let req, res, next;
 
     beforeEach(() => {
@@ -16,7 +16,7 @@ describe('config/reverseproxy.config.js', () => {
     it('normalises a decoded repository path', () => {
         req.params.repo = ['foo', 'bar', 'baz'];
 
-        reverseproxy.middleware(req, res, next);
+        decodeRepoPaths.middleware(req, res, next);
 
         expect(req.params.repo).to.equal('foo/bar/baz');
     });
@@ -24,13 +24,13 @@ describe('config/reverseproxy.config.js', () => {
     it('leaves encoded route repository params unchanged', () => {
         req.params.repo = 'foo/bar/baz';
 
-        reverseproxy.middleware(req, res, next);
+        decodeRepoPaths.middleware(req, res, next);
 
         expect(req.params.repo).to.equal('foo/bar/baz');
     });
 
     it('calls the next handler', () => {
-        reverseproxy.middleware(req, res, next);
+        decodeRepoPaths.middleware(req, res, next);
 
         expect(next).to.have.been.calledOnce;
     });
