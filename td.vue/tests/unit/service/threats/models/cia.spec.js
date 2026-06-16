16d3fa4c0d42ee39faee70595223e5bd4d1e5589
samiat4911 <Samiatakande@yahoo.com>
2026-06-16 12:16:01 +0100

    test: fix model translation assertions

--- td.vue/tests/unit/service/threats/models/cia.spec.js
+++ td.vue/tests/unit/service/threats/models/cia.spec.js
@@ -2,14 +2,14 @@ import cia from '@/service/threats/models/cia.js';
 
 describe('service/threats/models/cia.js', () => {
     it('has a translation for confidentiality', () => {
-        expect(cia.confidentiality.length).toBeGreaterThan(0);
+        expect(cia.confidentiality.translation.length).toBeGreaterThan(0);
     });
 
     it('has a translation for integrity', () => {
-        expect(cia.integrity.length).toBeGreaterThan(0);
+        expect(cia.integrity.translation.length).toBeGreaterThan(0);
     });
 
     it('has a translation for availability', () => {
-        expect(cia.availability.length).toBeGreaterThan(0);
+        expect(cia.availability.translation.length).toBeGreaterThan(0);
     });
 });
