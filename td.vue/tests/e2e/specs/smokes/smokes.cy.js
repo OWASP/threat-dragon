de9c681bfa1781200a1df258fb8c9d66b3b2cd48
Jamal <jamalkamaladdin@gmail.com>
2026-09-09 05:58:12 +0400

    update the locale count and word list for azerbaijani

--- td.vue/tests/e2e/specs/smokes/smokes.cy.js
+++ td.vue/tests/e2e/specs/smokes/smokes.cy.js
@@ -21,7 +21,7 @@ describe('smoke tests', () => {
         });
 
         it('shows locale dropdown with all available languages', () => {
-            const localesAvailableCount = 14;
+            const localesAvailableCount = 15;
 
             cy.get('.td-locale-select .td-dropdown-toggle').click();
             cy.get('.td-dropdown-scroll button').should('have.length', localesAvailableCount);
